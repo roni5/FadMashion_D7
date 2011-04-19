b62a7bcd2be15fe0d28f68c1a8132969da5afcfd
Anish Aggarwal <misternyce@gmail.com>
2011-04-19 13:09:55 -0400

    add new fm_invite module

---
+++ modules/custom/fm_invite/fm_invite_share.tpl.php
@@ -0,0 +1,5 @@
+<input type="text" value="<?php print $reg_code_url; ?>" id="share_url" 
+class="form-text-readonly" autocomplete="off"  style="font-size: 11px; width: 300px" name="share_url" readonly="readonly">
+
+<div style="margin: 10px 0;">Share </div>
+
