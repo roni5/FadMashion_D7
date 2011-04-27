2d9740cd0a2a89e0af73dbab70ac557202d2f368
Anish Aggarwal <misternyce@gmail.com>
2011-04-27 14:36:48 -0400

    Add Facebook module/sdk

--- modules/custom/fm_invite/fm_invite_share.tpl.php
+++ modules/custom/fm_invite/fm_invite_share.tpl.php
@@ -1,6 +1,5 @@
 <input type="text" value="<?php print $reg_code_url; ?>" id="share_url" 
 onClick="this.select()" class="form-text-readonly" autocomplete="off"  style=" width: 250px" name="share_url" readonly="readonly">
 
-<a href='http://www.facebook.com/share.php?u=<?php print $reg_code_url;?>'>
-<img alt='Facebook'src='http://i591.photobucket.com/albums/ss358/danishctc/blogres/fbook24.jpg'/></a>
+<?php print l("<img alt='Facebook' src='http://i591.photobucket.com/albums/ss358/danishctc/blogres/fbook24.jpg'/>", 'invite/facebook', array('html' => true)) ?> 
 
