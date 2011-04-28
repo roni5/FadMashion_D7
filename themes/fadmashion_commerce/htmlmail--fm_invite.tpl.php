0ec3aeea9d9819b4832bf8b9488cc5713943afa1
Anish Aggarwal <misternyce@gmail.com>
2011-04-28 19:44:33 -0400

    actual template

--- themes/fadmashion_commerce/htmlmail--fm_invite.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite.tpl.php
@@ -5,28 +5,33 @@
  * Sample template for HTML Mail test messages.
  */
 ?>
-<h1><a href="http://drupal.org/project/htmlmail">FM HTML Mail</a> test message</h1>
-<div class="htmlmail-body">
-<?php echo $body; ?>
-</div>
-<?php if ($debug): ?>
-<hr />
-<div class="htmlmail-debug">
-  <dl><dt><p>
-    To customize this test message:
-  </p></dt><dd><ol><li><p><?php if (empty($theme)): ?>
-    Visit <u>admin/config/system/htmlmail</u>
-    and select a theme to hold your custom email template files.
-  </p></dt><dd><ol><li><p><?php elseif (empty($theme_path)): ?>
-    Visit <u>admin/appearance</u>
-    to enable your selected <u><?php echo ucfirst($theme); ?></u> theme.
-  </p></dt><dd><ol><li><p><?php endif; ?>
-    Copy the
-    <a href="http://drupalcode.org/project/htmlmail.git/blob_plain/refs/heads/7.x-2.x:/htmlmail--htmlmail.tpl.php"><code>htmlmail--htmlmail.tpl.php</code></a>
-    file to your <u><?php echo ucfirst($theme); ?></u> theme directory
-    <u><code><?php echo $theme_path; ?></code></u>.
-  </p></li><li><p>
-    Edit the copied file.
-  </p></li></ol></dd></dl>
-</div>
-<?php endif;
+<table style="background:none repeat scroll 0 0 #FFFFFF;width:500px;" cellspacing="0" cellpadding="0" class="main"><tbody><tr>
+<td style="background:none repeat scroll 0 0 #FFFFFF;
+border-bottom:5px solid #BDB6AF;
+border-top:5px solid #BDB6AF;
+padding:25px;
+text-align:left;">
+
+<h1 style="font-size:22px;
+margin-top:0;">Excellent choice.</h1>
+
+<p style="font-size:13px;
+margin-bottom:7px;">We have received your request to shop the most sought after independent designers at FadMashion.com. To make the most out of FadMashion, learn about our social shopping rewards below. </p>	
+
+<h1>Your <span>Social Shopping Rewards</span> Link</h1> 
+<p>The link below was created just for you. Keep it safe and use it often. When you share your link and people click it, we will make sure you receive credit for your efforts at FadMashion.</p> 	
+
+	
+<div class="url"><p>Copy and paste this URL in Twitter, Facebook, or in an Email to invite friends.</p>
+<input type="text" style="padding: 4px 0pt 0pt 4px; width: 200px; margin-right: 10px;" value="http://fadmashion.com/mqjk5h" name=""> 	<span style="font-family: Georgia,'Times New Roman',Times,serif; font-style: italic;">share</span> 	 <a href="#"><img align="absbottom" class="social" src="facebook.png"></a><a href="#"><img align="absbottom" class="social" src="twitter.png"></a>	
+	</div>
+
+	
+<h1>Invite Friends</h1> 
+<a href="#">Check your status</a>
+
+
+<div class="logo"><a href="http://www.fadmashion.com"><img src="logo.jpg"></a></div>
+
+
+</td></tr></tbody></table>
