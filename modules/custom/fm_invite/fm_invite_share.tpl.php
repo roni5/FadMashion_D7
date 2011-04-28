27bb77f6daa3074014013b6cc25ee8cd5e52827a
Anish Aggarwal <misternyce@gmail.com>
2011-04-28 01:55:41 -0400

    text changes

--- modules/custom/fm_invite/fm_invite_share.tpl.php
+++ modules/custom/fm_invite/fm_invite_share.tpl.php
@@ -1,2 +1,3 @@
 <input type="text" value="<?php print $reg_code_url; ?>" id="share_url" 
 onClick="this.select()" class="form-text-readonly" autocomplete="off"  style=" width: 250px" name="share_url" readonly="readonly">
+<br clear="all">
