03e9353d2a04755b51818411bc6e4d6624725e7a
Anish Aggarwal <misternyce@gmail.com>
2011-04-28 20:08:54 -0400

    add full css template

--- themes/fadmashion_commerce/htmlmail--fm_invite.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite.tpl.php
@@ -5,45 +5,27 @@
  * Sample template for HTML Mail test messages.
  */
 ?>
+<table cellspacing="0" cellpadding="0" class="main" style="width: 500px; background: #fff;"><tbody><tr><td style="padding: 25px; background: #fff; border-top: 5px solid #bdb6af; border-bottom: 5px solid #bdb6af; text-align:left;">
 
-<style>
-*{font-family: helvetica, arial, trebuchet MS; font-size: 12px;	margin: 0; padding: 0; line-height: 16px; border: none; }
-h1{ font-size: 16px; margin: 20px 0 6px 0;}
-h1.big{font-size: 22px; margin-top: 0;}
-.main { width: 500px; background: #fff;}
-h1 span{color: #7b6858; font-size: 16px; }
-p{margin-bottom: 7px; font-size: 13px;}
-a{font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;}
-.url{padding: 13px; margin: 25px 0; background: #000; color: #fff; }
-.url p{font-size: 12px;}
-td{padding: 25px; background: #fff; border-top: 5px solid #bdb6af; border-bottom: 5px solid #bdb6af; text-align:left;}
-.logo{border-top: 1px solid #eae7e2; padding: 12px 0 0 0; margin-top: 25px;}
-.social{margin: 0px 3px}
-</style>
+<h1 style="font-size: 16px; margin: 20px 0 6px 0;">Excellent choice.</h1>
 
+<p style="font-size:13px;margin-bottom:7px;">We have received your request to shop the most sought after independent designers at FadMashion.com. To make the most out of FadMashion, learn about our social shopping rewards below. </p>	
 
-<table  cellspacing="0" cellpadding="0" class="main"><tbody><tr>
-<td >
-
-<h1>Excellent choice.</h1>
-
-<p style="font-size:13px;
-margin-bottom:7px;">We have received your request to shop the most sought after independent designers at FadMashion.com. To make the most out of FadMashion, learn about our social shopping rewards below. </p>	
-
-<h1>Your <span>Social Shopping Rewards</span> Link</h1> 
-<p>The link below was created just for you. Keep it safe and use it often. When you share your link and people click it, we will make sure you receive credit for your efforts at FadMashion.</p> 	
+<h1 style="font-size: 16px; margin: 20px 0 6px 0;">Your <span style="color: #7b6858; font-size: 16px;">Social Shopping Rewards</span> Link</h1> 
+<p style="margin-bottom: 7px; font-size: 13px;">The link below was created just for you. Keep it safe and use it often. When you share your link and people click it, we will make sure you receive credit for your efforts at FadMashion.</p> 	
 
 	
-<div class="url"><p>Copy and paste this URL in Twitter, Facebook, or in an Email to invite friends.</p>
-<input type="text" style="padding: 4px 0pt 0pt 4px; width: 200px; margin-right: 10px;" value="http://fadmashion.com/mqjk5h" name=""> 	<span style="font-family: Georgia,'Times New Roman',Times,serif; font-style: italic;">share</span> 	 <a href="#"><img align="absbottom" class="social" src="facebook.png"></a><a href="#"><img align="absbottom" class="social" src="twitter.png"></a>	
+<div class="url" style="padding: 13px; margin: 25px 0; background: #000; color: #fff;">
+<p style="margin-bottom:7px;font-size:12px;">Copy and paste this URL in Twitter, Facebook, or in an Email to invite friends.</p>
+<input type="text" style="padding: 4px 0pt 0pt 4px; width: 200px; margin-right: 10px;" value="http://fadmashion.com/mqjk5h" name=""><span style="font-family: Georgia,'Times New Roman',Times,serif; font-style: italic;">share</span> 	 <a href="#" style='font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img align="absbottom" class="social" src="facebook.png" style="margin: 0px 3px"></a><a href="#" style='font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img align="absbottom" class="social" src="twitter.png" style="margin: 0px 3px"></a>	
 	</div>
 
 	
-<h1>Invite Friends</h1> 
-<a href="#">Check your status</a>
+<h1 style="font-size: 16px; margin: 20px 0 6px 0;">Invite Friends</h1> 
+<a href="#" style='font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'>Check your status</a>
 
 
-<div class="logo"><a href="http://www.fadmashion.com"><img src="logo.jpg"></a></div>
+<div class="logo" style="border-top: 1px solid #eae7e2; padding: 12px 0 0 0; margin-top: 25px;"><a href="http://www.fadmashion.com" style='font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img src="logo.jpg"></a></div>
 
 
 </td></tr></tbody></table>
