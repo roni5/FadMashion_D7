4cea7d33cffb8e6497bde0cbfc37f13f4154ca38
Anish Aggarwal <misternyce@gmail.com>
2011-04-28 23:27:35 -0400

    more styling for e-mail

--- themes/fadmashion_commerce/htmlmail--fm_invite.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite.tpl.php
@@ -5,10 +5,10 @@
  * Sample template for HTML Mail test messages.
  */
 ?>
-<table cellspacing="0" cellpadding="0" class="main" style="margin-left: 200px; width: 500px; background: #fff;"><tbody><tr><td style="padding; 10px; background: #fff; border-top: 5px solid #bdb6af; border-bottom: 5px solid #bdb6af; text-align:left;">
+<table cellspacing="0" cellpadding="0" class="main" style="margin-left: 50px; width: 500px; background: #fff;"><tbody><tr><td style="padding; 10px; background: #fff; border-top: 5px solid #bdb6af; border-bottom: 5px solid #bdb6af; text-align:left;">
 
 
-<div class="logo" style="padding: 0 0 0 12px; margin-top: 25px;"><a href="http://www.fadmashion.com" style='font-size: 20px; font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img style=" border: 0px solid #000;" title="fadmashion" src="<?php print $theme_url?>/logo_black.jpg"></a></div>
+<div class="logo" style="padding: 0 0 12px 0; margin-top: 25px;"><a href="http://www.fadmashion.com" style='font-size: 20px; font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img style=" border: 0px solid #000;" title="fadmashion" src="<?php print $theme_url?>/logo_black.jpg"></a></div>
 
 
 <p style="font-size:13px;margin-bottom:7px;">We have received your request to shop the most sought after independent designers at FadMashion.com. To make the most out of FadMashion, learn about our social shopping rewards below. </p>	
