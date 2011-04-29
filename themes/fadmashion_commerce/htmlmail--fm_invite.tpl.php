f951b7ded40e5f9eed3e331ca6dadbab990cf814
Anish Aggarwal <misternyce@gmail.com>
2011-04-29 10:49:14 -0400

    text change

--- themes/fadmashion_commerce/htmlmail--fm_invite.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite.tpl.php
@@ -11,10 +11,10 @@
 <div class="logo" style="padding: 0 0 12px 0; margin-top: 25px;"><a href="http://www.fadmashion.com" style='font-size: 20px; font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img style=" border: 0px solid #000;" title="fadmashion" src="<?php print $theme_url?>/logo_black.jpg"></a></div>
 
 
-<p style="font-size:13px;margin-bottom:7px;">We have received your request to shop the most sought after independent designers at FadMashion.com. To make the most out of FadMashion, learn about our social shopping rewards below. </p>	
+<p style="font-size:13px;margin-bottom:7px;">We have received your request to shop up to 60% off the best independent designers worldwide. We will be in touch with your personally invitation following our launch. To speed things up, learn about your FM Link and Social Shopping Rewards.</p>	
 
-<h1 style="font-size: 16px; margin: 20px 0 6px 0;">Your <span style="margin-left: 5px; color: #7b6858; font-size: 16px;">Social Shopping Rewards</span> Link</h1> 
-<p style="margin-bottom: 7px; font-size: 13px;">The link below was created just for you. Keep it safe and use it often. When you share your link and people click it, we will make sure you receive credit for your efforts at FadMashion.</p> 	
+<h1 style="font-size: 16px; margin: 20px 0 6px 0;">Your <span style="margin-left: 3px; color: #7b6858; font-size: 16px;">Social Shopping Rewards</span> Link</h1> 
+<p style="margin-bottom: 7px; font-size: 13px;">As our community grows, we will release new social rewards and your Social Shopping Rewards Link will gain more value.  Keep your link safe, and share it often. Your first reward opportunity lets you jump the line. Share your link with 5 friends who sign up and you'll be among the first to step inside.</p> 	
 
 	
 <div class="url" style="padding: 13px; margin: 25px 0; background: #000; color: #fff;">
