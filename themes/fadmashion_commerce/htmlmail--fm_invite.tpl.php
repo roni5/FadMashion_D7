5b2ad923cb2ca8a79849bbe5d921d57cd790c903
Anish Aggarwal <misternyce@gmail.com>
2011-04-28 22:28:22 -0400

    add reg_code

--- themes/fadmashion_commerce/htmlmail--fm_invite.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite.tpl.php
@@ -5,7 +5,7 @@
  * Sample template for HTML Mail test messages.
  */
 ?>
-<table cellspacing="0" cellpadding="0" class="main" style="width: 500px; background: #fff;"><tbody><tr><td style="padding: 25px; background: #fff; border-top: 5px solid #bdb6af; border-bottom: 5px solid #bdb6af; text-align:left;">
+<table cellspacing="0" cellpadding="0" class="main" style="width: 500px; background: #fff;"><tbody><tr><td style="background: #fff; border-top: 5px solid #bdb6af; border-bottom: 5px solid #bdb6af; text-align:left;">
 
 <h1 style="font-size: 22px; margin: 20px 0 6px 0;">Excellent choice.</h1>
 
@@ -17,7 +17,7 @@
 	
 <div class="url" style="padding: 13px; margin: 25px 0; background: #000; color: #fff;">
 <p style="margin-bottom:7px;font-size:12px;">Copy and paste this URL in Twitter, Facebook, or in an Email to invite friends.</p>
-<input type="text" style="padding: 4px 0pt 0pt 4px; width: 200px; margin-right: 10px;" value="http://fadmashion.com/mqjk5h" name=""><span style="font-family: Georgia,'Times New Roman',Times,serif; font-style: italic;">share</span> 	 <a href="#" style='font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img align="absbottom" class="social" src="facebook.png" style="margin: 0px 3px"></a><a href="#" style='font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img align="absbottom" class="social" src="twitter.png" style="margin: 0px 3px"></a>	
+<input type="text" style="padding: 4px 0pt 0pt 4px; width: 200px; margin-right: 10px;" value="http://fadmashion.com/<?php  ?>" name="">	
 	</div>
 
 	
@@ -25,7 +25,7 @@
 <a href="#" style='font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'>Check your status</a>
 
 
-<div class="logo" style="border-top: 1px solid #eae7e2; padding: 12px 0 0 0; margin-top: 25px;"><a href="http://www.fadmashion.com" style='font-size: 20px; font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img title="fadmashion" src="http://www.fadmashion.com/<?php print url(drupal_get_path("theme","fadmashion_commerce");?>/images/logo.png"></a></div>
+<div class="logo" style="border-top: 1px solid #eae7e2; padding: 12px 0 0 0; margin-top: 25px;"><a href="http://www.fadmashion.com" style='font-size: 20px; font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img title="fadmashion" src="<?php print $theme_url?>/images/logo.png"></a></div>
 
 
 </td></tr></tbody></table>
