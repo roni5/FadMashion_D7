3a77ead757be2315eaeb5ac38341054ad6e00b83
Anish Aggarwal <misternyce@gmail.com>
2011-04-28 22:08:41 -0400

    template change

--- themes/fadmashion_commerce/htmlmail--fm_invite.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite.tpl.php
@@ -7,7 +7,7 @@
 ?>
 <table cellspacing="0" cellpadding="0" class="main" style="width: 500px; background: #fff;"><tbody><tr><td style="padding: 25px; background: #fff; border-top: 5px solid #bdb6af; border-bottom: 5px solid #bdb6af; text-align:left;">
 
-<h1 style="font-size: 16px; margin: 20px 0 6px 0;">Excellent choice.</h1>
+<h1 style="font-size: 22px; margin: 20px 0 6px 0;">Excellent choice.</h1>
 
 <p style="font-size:13px;margin-bottom:7px;">We have received your request to shop the most sought after independent designers at FadMashion.com. To make the most out of FadMashion, learn about our social shopping rewards below. </p>	
 
@@ -25,7 +25,7 @@
 <a href="#" style='font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'>Check your status</a>
 
 
-<div class="logo" style="border-top: 1px solid #eae7e2; padding: 12px 0 0 0; margin-top: 25px;"><a href="http://www.fadmashion.com" style='font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img src="logo.jpg"></a></div>
+<div class="logo" style="border-top: 1px solid #eae7e2; padding: 12px 0 0 0; margin-top: 25px;"><a href="http://www.fadmashion.com" style='font-size: 20px; font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img title="fadmashion" src="<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/logo.png"></a></div>
 
 
 </td></tr></tbody></table>
