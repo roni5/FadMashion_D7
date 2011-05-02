e9ef5bfa99069233b9e2a37b950979be70b9a8e5
Anish Aggarwal <misternyce@gmail.com>
2011-05-02 15:05:12 -0400

    fb invite

--- modules/custom/fm_invite/fm_invite.js
+++ modules/custom/fm_invite/fm_invite.js
@@ -6,7 +6,7 @@ function fm_invite_facebookshare() {
 	 FB.ui(
 			   {
 			     method: 'feed',
-			     app_id= '213872101957329',
+			     app_id: '213872101957329',
 			     name: 'Facebook Dialogs',
 			     link: 'http://www.fadmashion.com/',
 			     picture: 'http://fbrell.com/f8.jpg',
@@ -14,13 +14,6 @@ function fm_invite_facebookshare() {
 			     description: 'Dialogs provide a simple, consistent interface for applications to interface with users.',
 			     message: 'Facebook Dialogs are easy!'
 			   },
-			   function(response) {
-			     if (response && response.post_id) {
-			       alert('Post was published.');
-			     } else {
-			       alert('Post was not published.');
-			     }
-			   }
 			 );
 
 }
