f190c4d6c93ff84a1a8e788fc350b1c50f396938
Anish Aggarwal <misternyce@gmail.com>
2011-05-09 10:26:07 -0400

    add invite permissions and styling changes

--- themes/fadmashion_commerce/js/FM_form.js
+++ themes/fadmashion_commerce/js/FM_form.js
@@ -3,21 +3,37 @@
  */
 
 jQuery(document).ready(function() {
+	clearAllForms();
+});
 
-	jQuery.fn.cleardefault = function() {
-	    return this.focus(function() {
-		if( this.value == this.defaultValue ) {
-			this.value = "";
-		}
-	}).blur(function() {
-		if( !this.value.length ) {
-			this.value = this.defaultValue;
-		}
-	});
+function clearAllForms() {
+  //Call the Form clear and restore
+  jQuery("input.form-text").cleardefault();
+  jQuery("input.form-textarea").cleardefault();
 };
 
-
-//Call the Form clear and restore
-jQuery("input.form-text").cleardefault();
-
+jQuery.fn.cleardefault = function() {
+    return this.focus(function() {
+	if( this.value == this.defaultValue ) {
+		this.value = "";
+	}
+}).blur(function() {
+	if( !this.value.length ) {
+		this.value = this.defaultValue;
+	}
 });
+};
+
+(function ($) {
+	Drupal.behaviors.addClearForms = {
+	  attach: function (context, settings) {
+	    if (!$.isFunction($.colorbox)) {
+	      return;
+	    }
+	    clearAllForms();
+	  }
+	};
+})(jQuery);
+
+
+
