5353ee5de4490f0710d236254fbc7e6d81090176
Anish Aggarwal <misternyce@gmail.com>
2011-05-11 00:00:25 -0400

    email friends template

--- themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
@@ -7,6 +7,6 @@
 ?>
 
 <table cellspacing="0" cellpadding="0" class="main" style="margin-left: 50px; width: 500px; background: #fff;"><tbody><tr><td style="padding; 10px; background: #fff; border-top: 5px solid #bdb6af; text-align:left;">
-<div class="logo" style="padding: 0 0 12px 0; margin-top: 25px;"><a href="http://www.fadmashion.com" style='font-size: 20px; font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img style=" border: 0px solid #000;" title="fadmashion" src="<?php print $theme_url?>/logo_black.jpg"></a></div>
+<div style="font-family: Helvetica; font-size: 24px;color:#000;margin: 20px 0">fad<b>mashion</b></div>
 <div><?php print $body;?></div>
 </td></tr></tbody></table>
