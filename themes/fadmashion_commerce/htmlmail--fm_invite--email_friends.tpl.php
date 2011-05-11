8e111c9eb637be4ae51db7bb1489a5bf05fffed7
Anish Aggarwal <misternyce@gmail.com>
2011-05-11 00:04:05 -0400

    html email invite

--- themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
@@ -6,7 +6,7 @@
  */
 ?>
 
-<table cellspacing="0" cellpadding="0" class="main" style="margin-left: 50px; width: 500px; background: #fff;"><tbody><tr><td style="padding; 10px; background: #fff; border-top: 5px solid #bdb6af; text-align:left;">
-<div style="font-family: Helvetica; font-size: 24px;color:#000;margin: 20px 0">fad<b>mashion</b></div>
+<table cellspacing="0" cellpadding="0" class="main" style="margin-left: 10px; width: 500px; background: #fff;"><tbody><tr><td style="padding; 10px; background: #fff; border-top: 5px solid #bdb6af; text-align:left;">
+<div style="font-family: Helvetica; font-size: 34px;color:#000;margin: 3px 0 10px">fad<b>mashion</b></div>
 <div><?php print $body;?></div>
 </td></tr></tbody></table>
