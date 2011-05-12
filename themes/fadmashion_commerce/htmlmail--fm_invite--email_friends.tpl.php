46a3b10bbf8030386762bcb53c2cfee463b0f603
Anish Aggarwal <misternyce@gmail.com>
2011-05-12 12:02:54 -0400

    change copy

--- themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
@@ -8,6 +8,6 @@
 
 <table cellspacing="0" cellpadding="0" class="main" style="margin-left: 10px; width: 500px; background: #fff;"><tbody><tr><td style="padding; 10px; background: #fff; border-top: 5px solid #bdb6af; text-align:left;">
 <div style="margin-top: 10px;"><?php print $body;?></div>
-<div style="font-family: Helvetica; font-style: italics; font-size: 14px;color:#888;margin: 15px 0 5px"><?php print l('Follow Fadmashion on Tumblr', 'http://blog.fadmashion.com', array('absolute' => true))?> and see the latest trends in independent fashion</div>
+<div style="font-family: Helvetica; font-size: 12px;color:#888;margin: 15px 0 5px"><?php print l('Follow Fadmashion\'s blog', 'http://blog.fadmashion.com', array('absolute' => true))?> to get daily inspirations on independent styles</div>
 
 </td></tr></tbody></table>
