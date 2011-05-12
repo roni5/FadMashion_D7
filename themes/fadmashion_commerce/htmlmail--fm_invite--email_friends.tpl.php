10569a892380004bb286f34497046c3ac0e0bf67
Anish Aggarwal <misternyce@gmail.com>
2011-05-12 11:38:37 -0400

    invite styling

--- themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
@@ -7,7 +7,7 @@
 ?>
 
 <table cellspacing="0" cellpadding="0" class="main" style="margin-left: 10px; width: 500px; background: #fff;"><tbody><tr><td style="padding; 10px; background: #fff; border-top: 5px solid #bdb6af; text-align:left;">
-<div><?php print $body;?></div>
+<div style="margin-top: 10px;"><?php print $body;?></div>
 <div style="font-family: Helvetica; font-size: 34px;color:#000;margin: 15px 0 5px">
    <a href="http://www.fadmashion.com" style='font-size: 20px; font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img style=" border: 0px solid #000;" title="fadmashion" src="<?php print $theme_url?>/logo_black.jpg">
    </a>
