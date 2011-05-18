8111bee383083a1e73667dde7176a3cd667c7830
Anish Aggarwal <misternyce@gmail.com>
2011-05-18 12:08:30 -0400

    change copy

--- themes/fadmashion_commerce/htmlmail--fm_invite--confirmation.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite--confirmation.tpl.php
@@ -16,9 +16,10 @@
 
 <div class="pad" style="padding: 24px 15px 10px 50px">
 <h1 style="font-size:24px;width:auto;margin:4px 0;">Excellent choice.</h1>
-<p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">As a loyal member, Fadmashion is offering you priority access to our upcoming private launch as well as the opportunity to get free shipping for your first 20 orders (up to a<b> $200 value</b>). </p>
+<p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">As a loyal early member, Fadmashion is offering you priority access to our upcoming private launch as well as the opportunity to get free shipping for your first 20 orders (up to a<b> $200 value</b>). </p>
 <br><h1 style="width: auto; font-size: 18px; margin: 4px 0;">Your Social Shopping Rewards Link</h1>
-<p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">The link below was created just for you. Keep it safe and use it often. When you share your link and your friends sign up, you will be rewarded.</p>
+<p style="margin: 0 0 10px 0;  font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">The link below is your very own, and it will <b>earn you Social Shopping Rewards</b> - share your link with friends and earn priority access, free shipping, exclusive parties and much more.  Keep your link safe, and share it often. </p> 	
+<p style="margin: 0 0 10px 0;  font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">Your first reward opportunity lets you jump the line. Share the link below with at least <b>5 friends</b> who sign up and you'll be among the first to step inside.</p>
 
 <div class="url" style="background: #d7d1c7; padding: 15px; margin: 20px 0;">
 <p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">Copy and paste this URL in Twitter, Facebook,<br>
@@ -38,10 +39,5 @@
 </tr>
 </table>
 
-<div style="color:#796155;font-family:Georgia,"Times New Roman",Times,serif;font-size:13px;font-style:italic;line-height:20px;padding:80px 0 0;" class="quote">
- "FadMashion.com has changed the way you'll shop for independent fashion from now on."
- <img align="right" style="display: block; margin-top: 14px;" alt="-myfashionlife" src="myfashionlife.jpg">
- </div>
- 
 </div> 
 
