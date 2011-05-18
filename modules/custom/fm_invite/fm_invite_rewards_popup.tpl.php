8e2064f60cf8075584dd68eb155c44230ddbaa65
Anish Aggarwal <misternyce@gmail.com>
2011-05-18 13:12:34 -0400

    rewards popup issue

--- modules/custom/fm_invite/fm_invite_rewards_popup.tpl.php
+++ modules/custom/fm_invite/fm_invite_rewards_popup.tpl.php
@@ -1,4 +1,5 @@
 
+<h2 class="socialSubtitle">Share your Link</h2>
 <div class="share">
 
 	<div class="social">
@@ -9,29 +10,27 @@
     print l($image, 'colorbox/form/fm_invite_send_email_form', array( 'html' => true, 'query' => array('id' => $_GET['id']), 'attributes' => array('class' => 'colorbox-load socialIcon', 'title' => 'Spread the word') )  ); 
    ?>
    </div>
-
-    <div class="rewards_link"><input type="text" readonly="readonly" name="share_url" style="width: 250px; border: 1px solid rgb(197, 189, 182); color: rgb(114, 101, 92); float: left; margin: 0pt; padding: 2px 0pt 0pt 2px; height: 22px;" autocomplete="off" class="form-text-readonly" onclick="this.select()" id="share_url" value="http://exp.fadmashion.com/invite/U3E8dyHk">
-    </div>
+<div class="rewards_link"><input type="text" readonly="readonly" name="share_url" style="width: 250px; border: 1px solid rgb(197, 189, 182); color: rgb(114, 101, 92); float: left; margin: 0pt; padding: 2px 0pt 0pt 2px; height: 22px;" autocomplete="off" class="form-text-readonly" onclick="this.select()" id="share_url" value="http://exp.fadmashion.com/invite/U3E8dyHk"></div>
 </div>
 
 <br clear="all">
 <div class="signupBox" id="signup5">
 <h1>5</h1>
-<h2>SIGN-UPS</h2>
+<h3>SIGN-UPS</h2>
 <p>Priority Access and Free shipping for first purchase</p>
 <p class="value">A $10 value</p>
 </div>
 
 <div class="signupBox" id="signup15">
 <h1>15</h1>
-<h2>SIGN-UPS</h2>
+<h3>SIGN-UPS</h2>
 <p>Priority Access and Free shipping for first 10 purchases</p>
 <p class="value">A $100 value</p>
 </div>
 
 <div class="signupBox" id="signup25">
 <h1>25</h1>
-<h2>SIGN-UPS</h2>
+<h3>SIGN-UPS</h2>
 <p>Priority Access and Free shipping for first 20 purchases</p>
 <p class="value">A $200 value</p>
 </div>
