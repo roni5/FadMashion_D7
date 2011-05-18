1e38df8194c61a9f7c1b5fa9946acd578ef36554
Anish Aggarwal <misternyce@gmail.com>
2011-05-18 11:46:42 -0400

    add email image files

--- themes/fadmashion_commerce/htmlmail--fm_invite--confirmation.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite--confirmation.tpl.php
@@ -6,10 +6,10 @@
  */
 ?>
 
-<div class="wrapper" style="background-color: #221e1b; padding: 20px 0 0 0;">
+<div class="wrapper" style="background-color: #221e1b; padding: 20px 0 0 20px; margin-bottom: 10px;">
 <table cellpadding="0" cellspacing="0" width="600" style="margin: 0 auto;">
 <tr>
-<td colspan="2"><img src="http://www.owenlinton.com/fadmashion/dev/email1/header.jpg" style="border: none; display: block;"></td>
+<td colspan="2"><img title = "fadmashion" src="<?php print $theme_url?>/images/template1/header.jpg" style="border: none; display: block;"></td>
 </tr>
 <tr>
 <td class="content" valign="top" style="background-color: #efeee9;">
@@ -24,22 +24,24 @@
 <p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">Copy and paste this URL in Twitter, Facebook,<br>
  or in an Email to invite friends.</p>
 
-<input name="" type="text" value="<?php print $body; ?>" style="padding: 1px 0 0 4px; height: 18px; width: 200px; margin-right: 10px;"><span style="font-family: Georgia, 'Times New Roman', Times, serif; font-style:italic; font-size: 13px;">share</span> 	 <a href="#" style="color: #01517e;"><img src="http://www.owenlinton.com/fadmashion/dev/email1/facebook.png" align="absmiddle" class="social" style="border:none;display:inline;"></a> <a href="<?php print theme('fm_invite_twitter_text' , '');?>" style="color: #01517e;"><img src="http://www.owenlinton.com/fadmashion/dev/email1/twitter.png" align="absmiddle" class="social" style="border:none;display:inline;"></a>	
+<input name="" type="text" value="<?php print $body; ?>" style="padding: 1px 0 0 4px; height: 18px; width: 200px; margin-right: 10px;">
 	</div>
 
 </div>
 </td>
 
-<td width="195"><img src="http://www.owenlinton.com/fadmashion/dev/email1/model.jpg" style="border: none; display: block;"></td>
+<td width="195"><img src="<?php print $theme_url?>/images/template1/model.jpg" style="border: none; display: block;"></td>
 </tr>
 <tr>
-<td colspan="2"><img src="http://www.owenlinton.com/fadmashion/dev/email1/footer.jpg" style="border: none; display: block;"></td>
+<td colspan="2"><img src="<?php print $theme_url?>/images/template1/footer.jpg" style="border: none; display: block;"></td>
 
 </tr>
 </table>
-</div> 
 
 <div style="color:#796155;font-family:Georgia,"Times New Roman",Times,serif;font-size:13px;font-style:italic;line-height:20px;padding:80px 0 0;" class="quote">
  "FadMashion.com has changed the way you'll shop for independent fashion from now on."
  <img align="right" style="display: block; margin-top: 14px;" alt="-myfashionlife" src="myfashionlife.jpg">
  </div>
+ 
+</div> 
+
