2aed49df171d4759236aace3f6c3545b67735b5e
Anish Aggarwal <misternyce@gmail.com>
2011-05-18 00:15:15 -0400

    change copy

--- themes/fadmashion_commerce/htmlmail--fm_invite--confirmation.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite--confirmation.tpl.php
@@ -15,10 +15,10 @@
 <td class="content" valign="top" style="background-color: #efeee9;">
 
 <div class="pad" style="padding: 24px 15px 10px 50px">
-<h1 style="font-size:18px;width:auto;margin:4px 0;">Excellent choice.</h1>
-<p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">We have received your request to shop the most sought after independent designers at <a href="http://www.fadmashion.com" style="color: #01517e;">FadMashion.com</a>. To make the most out of FadMashion, learn about our social shopping rewards below.</p>
+<h1 style="font-size:24px;width:auto;margin:4px 0;">Excellent choice.</h1>
+<p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">We have received your request to shop up to 60% off the best independent designers worldwide and will be in touch with you following our launch.  To be one of the first members inside, take a moment to learn about Fadmashion Social Shopping Rewards.</p>
 <br><h1 style="width: auto; font-size: 18px; margin: 4px 0;">Your Social Shopping Rewards Link</h1>
-<p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">The link below was created just for you. Keep it safe and use it often. When you share your link and people click it, we will make sure you receive credit for your efforts at FadMashion.</p>
+<p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">The link below was created just for you. Keep it safe and use it often. When you share your link and your friends sign up, you will be rewarded.</p>
 
 <div class="url" style="background: #d7d1c7; padding: 15px; margin: 20px 0;">
 <p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">Copy and paste this URL in Twitter, Facebook,<br>
@@ -38,3 +38,8 @@
 </tr>
 </table>
 </div> 
+
+<div style="color:#796155;font-family:Georgia,"Times New Roman",Times,serif;font-size:13px;font-style:italic;line-height:20px;padding:80px 0 0;" class="quote">
+ "FadMashion.com has changed the way you'll shop for independent fashion from now on."
+ <img align="right" style="display: block; margin-top: 14px;" alt="-myfashionlife" src="myfashionlife.jpg">
+ </div>
