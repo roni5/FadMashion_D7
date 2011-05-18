514a6c903437b2304be770f6841da1e673d2d00e
Anish Aggarwal <misternyce@gmail.com>
2011-05-18 16:26:46 -0400

    adding images

--- themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
@@ -16,12 +16,12 @@
 
 <div class="pad" style="padding: 24px 15px 10px 50px">
 <h1 style="font-size:18px;width:auto;margin:9px 0;">Exciting fashion find!</h1>
-<p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">I just requested an invitation to Fadmashion's upcoming private launch which is offering up to 60% off independent designs. Sign up too and we both get priority access!</p>
+<p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;"><?php print $body;?></p>
 
 <div class="signUp" style="float: left; display: block; margin: 20px 0;">
-<img src="<?php print $theme_url?>/images/db_left.jpg" class="buttonEdge" style="border:none;display:block;float:left;"><a href="#" class="button" style="color:#fff;height:54px;font-size:19px;line-height:54px;padding:0 20px;background:#937c72;text-decoration:none;float:left;">Sign up to Fadmashion</a><img src="<?php print $theme_url?>/images/b_right.jpg" class="buttonEdge" style="border:none;display:block;float:left;">
+<img src="<?php print $theme_url?>/images/db_left.jpg" class="buttonEdge" style="border:none;display:block;float:left;"><a href="<?php print $params['reg_url'];?>" class="button" style="color:#fff;height:54px;font-size:19px;line-height:54px;padding:0 20px;background:#937c72;text-decoration:none;float:left;">Sign up to Fadmashion</a><img src="<?php print $theme_url?>/images/b_right.jpg" class="buttonEdge" style="border:none;display:block;float:left;">
 </div>
-<br clear="all"><p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;"><?php print var_dump($params);?></p>
+<br clear="all"><p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;"><?php print $params['fromName'];?></p>
 
 <div class="quote" style='padding: 80px 0 0 0; color: #796155; font-family:Georgia, "Times New Roman", Times, serif; font-style:italic; font-size: 13px; line-height: 20px;'>
  "FadMashion.com has changed the way you'll shop for independent fashion from now on."
