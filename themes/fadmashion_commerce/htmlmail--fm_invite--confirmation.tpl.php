2901103f60f57c07a00b8bbd07bce2946d818c9d
Anish Aggarwal <misternyce@gmail.com>
2011-05-18 12:11:40 -0400

    copy change

--- themes/fadmashion_commerce/htmlmail--fm_invite--confirmation.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite--confirmation.tpl.php
@@ -6,7 +6,7 @@
  */
 ?>
 
-<div class="wrapper" style="background-color: #221e1b; padding: 20px 0 0 20px; margin-bottom: 10px;">
+<div class="wrapper" style="background-color: #221e1b; padding: 20px 0 20px 0; margin-bottom: 10px;">
 <table cellpadding="0" cellspacing="0" width="600" style="margin: 0 auto;">
 <tr>
 <td colspan="2"><img title = "fadmashion" src="<?php print $theme_url?>/images/template1/header.jpg" style="border: none; display: block;"></td>
@@ -18,14 +18,14 @@
 <h1 style="font-size:24px;width:auto;margin:4px 0;">Excellent choice.</h1>
 <p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">As a loyal early member, Fadmashion is offering you priority access to our upcoming private launch as well as the opportunity to get free shipping for your first 20 orders (up to a<b> $200 value</b>). </p>
 <br><h1 style="width: auto; font-size: 18px; margin: 4px 0;">Your Social Shopping Rewards Link</h1>
-<p style="margin: 0 0 10px 0;  font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">The link below is your very own, and it will <b>earn you Social Shopping Rewards</b> - share your link with friends and earn priority access, free shipping, exclusive parties and much more.  Keep your link safe, and share it often. </p> 	
+<p style="margin: 0 0 10px 0;  font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">The link below is your very own, and it will earn you Social Shopping Rewards - share your link with friends and earn <b>priority access, free shipping, exclusive parties</b> and much more.  Keep your link safe, and share it often. </p> 	
 <p style="margin: 0 0 10px 0;  font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">Your first reward opportunity lets you jump the line. Share the link below with at least <b>5 friends</b> who sign up and you'll be among the first to step inside.</p>
 
 <div class="url" style="background: #d7d1c7; padding: 15px; margin: 20px 0;">
 <p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">Copy and paste this URL in Twitter, Facebook,<br>
  or in an Email to invite friends.</p>
 
-<input name="" type="text" value="<?php print $body; ?>" style="padding: 1px 0 0 4px; height: 18px; width: 200px; margin-right: 10px;">
+<input name="" type="text" value="<?php print $body; ?>" style="padding: 1px 0 0 4px; height: 18px; width: 290px; margin-right: 10px;">
 	</div>
 
 </div>
