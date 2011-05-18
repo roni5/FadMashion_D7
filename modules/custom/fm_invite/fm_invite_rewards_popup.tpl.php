1ec75cc3a53cf797cb90b456ce3a1dc781170601
Anish Aggarwal <misternyce@gmail.com>
2011-05-18 12:52:35 -0400

    change copy

--- modules/custom/fm_invite/fm_invite_rewards_popup.tpl.php
+++ modules/custom/fm_invite/fm_invite_rewards_popup.tpl.php
@@ -3,7 +3,7 @@
 
 	<div class="social">
   <a class="socialIcon" href="javascript:void(0);" onClick="javascript:fm_invite_facebookshare();"><img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/icon_facebook_white.jpg"></a>
-  <a class="socialIcon" href="target="_blank" href="<?php print theme('fm_invite_twitter_text', '')?>"><img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/icon_twitter_white.jpg"></a>
+  <a class="socialIcon" href="target="_blank" href="<?php print theme( 'fm_invite_twitter_text','');?>"><img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/icon_twitter_white.jpg"></a>
   <?php 
     $image = '<img src="/' . drupal_get_path("theme","fadmashion_commerce") . '/images/icon_mail_white.jpg"><br>Email';
     print l($image, 'colorbox/form/fm_invite_send_email_form', array( 'html' => true, 'query' => array('id' => $_GET['id']), 'attributes' => array('class' => 'colorbox-load socialIcon', 'title' => 'Spread the word') )  ); 
@@ -11,7 +11,7 @@
    </div>
 
     <div class="rewards_link"><input type="text" readonly="readonly" name="share_url" style="width: 250px; border: 1px solid rgb(197, 189, 182); color: rgb(114, 101, 92); float: left; margin: 0pt; padding: 2px 0pt 0pt 2px; height: 22px;" autocomplete="off" class="form-text-readonly" onclick="this.select()" id="share_url" value="http://exp.fadmashion.com/invite/U3E8dyHk">
-    <input type="submit" style="float: left; position: relative; left: -5px;" class="form-submit" value="Copy" name="op" id="edit-submit"></div>
+    </div>
 </div>
 
 <br clear="all">
