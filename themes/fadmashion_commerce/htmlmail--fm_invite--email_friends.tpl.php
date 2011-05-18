36388feb9cc5694dadf3c6177426e3488866829b
Anish Aggarwal <misternyce@gmail.com>
2011-05-18 14:27:35 -0400

    e-mail template when you send to friends

--- themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
@@ -2,12 +2,43 @@
 
 /**
  * @file
- * Sample template for HTML Mail test messages.
+ * Template when sending a message to friends.
  */
 ?>
 
-<table cellspacing="0" cellpadding="0" class="main" style="margin-left: 10px; width: 500px; background: #fff;"><tbody><tr><td style="padding; 10px; background: #fff; border-top: 5px solid #bdb6af; text-align:left;">
-<div style="margin-top: 10px;"><?php print $body;?></div>
-<div style="font-family: Helvetica; font-size: 12px;color:#888;margin: 15px 0 5px"><?php print l('Follow Fadmashion\'s blog', 'http://blog.fadmashion.com', array('absolute' => true))?> to get daily inspirations on independent styles</div>
+<div class="wrapper" align="center" style="background-color: #221e1b; padding: 20px 0 ;">
+<table cellpadding="0" cellspacing="0" width="600" style="margin: 0 auto;">
+<tr>
+<td colspan="2"><img src="http://www.owenlinton.com/fadmashion/dev/email2/header.jpg" alt="FadMashion" style="border: none; display: block;"></td>
+</tr>
+<tr>
+<td class="content" valign="top" style="background-color: #f0f0e8;">
 
-</td></tr></tbody></table>
+<div class="pad" style="padding: 24px 15px 10px 50px">
+<h1 style="font-size:18px;width:auto;margin:9px 0;">Exciting fashion find!</h1>
+<p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">I just requested an invitation to Fadmashion's upcoming private launch which is offering up to 60% off independent designs. Sign up too and we both get priority access!</p>
+
+<div class="signUp" style="float: left; display: block; margin: 20px 0;">
+<img src="b_left.jpg" class="buttonEdge" style="border:none;display:block;float:left;"><a href="#" class="button" style="color:#fff;height:54px;font-size:19px;line-height:54px;padding:0 20px;background:#937c72;text-decoration:none;float:left;">Sign up to Fadmashion</a><img src="b_right.jpg" class="buttonEdge" style="border:none;display:block;float:left;">
+</div>
+<br clear="all"><p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">- Owen</p>
+
+<div class="quote" style='padding: 80px 0 0 0; color: #796155; font-family:Georgia, "Times New Roman", Times, serif; font-style:italic; font-size: 13px; line-height: 20px;'>
+ "FadMashion.com has changed the way you'll shop for independent fashion from now on."
+ <img src="http://www.owenlinton.com/fadmashion/dev/email2/myfashionlife.jpg" alt="-myfashionlife" align="right" style="display:block;margin-top:14px;border:none;">
+</div>
+
+
+
+</div>
+</td>
+
+<td width="237"><img src="http://www.owenlinton.com/fadmashion/dev/email2/model.jpg" style="border: none; display: block;"></td>
+</tr>
+<tr>
+<td colspan="2"><img src="http://www.owenlinton.com/fadmashion/dev/email2/footer.jpg" style="border: none; display: block;"></td>
+
+</tr>
+</table>
+</div>
+
