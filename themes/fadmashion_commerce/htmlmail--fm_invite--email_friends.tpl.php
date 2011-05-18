1f8c95e867702a5b489ae63d5586a881fda4a575
Anish Aggarwal <misternyce@gmail.com>
2011-05-18 16:23:37 -0400

    dump e-mail vars

--- themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite--email_friends.tpl.php
@@ -9,7 +9,7 @@
 <div class="wrapper" align="center" style="background-color: #221e1b; padding: 20px 0 ;">
 <table cellpadding="0" cellspacing="0" width="600" style="margin: 0 auto;">
 <tr>
-<td colspan="2"><img src="http://www.owenlinton.com/fadmashion/dev/email2/header.jpg" alt="Fadmashion" style="border: none; display: block;"></td>
+<td colspan="2"><img src="<?php print $theme_url?>/images/template2/header.jpg" alt="Fadmashion" style="border: none; display: block;"></td>
 </tr>
 <tr>
 <td class="content" valign="top" style="background-color: #f0f0e8;">
@@ -19,13 +19,13 @@
 <p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">I just requested an invitation to Fadmashion's upcoming private launch which is offering up to 60% off independent designs. Sign up too and we both get priority access!</p>
 
 <div class="signUp" style="float: left; display: block; margin: 20px 0;">
-<img src="http://www.owenlinton.com/fadmashion/dev/email2/b_left.jpg" class="buttonEdge" style="border:none;display:block;float:left;"><a href="#" class="button" style="color:#fff;height:54px;font-size:19px;line-height:54px;padding:0 20px;background:#937c72;text-decoration:none;float:left;">Sign up to Fadmashion</a><img src="http://www.owenlinton.com/fadmashion/dev/email2/b_right.jpg" class="buttonEdge" style="border:none;display:block;float:left;">
+<img src="<?php print $theme_url?>/images/db_left.jpg" class="buttonEdge" style="border:none;display:block;float:left;"><a href="#" class="button" style="color:#fff;height:54px;font-size:19px;line-height:54px;padding:0 20px;background:#937c72;text-decoration:none;float:left;">Sign up to Fadmashion</a><img src="<?php print $theme_url?>/images/b_right.jpg" class="buttonEdge" style="border:none;display:block;float:left;">
 </div>
-<br clear="all"><p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;">- Owen</p>
+<br clear="all"><p style="margin: 0 0 10px 0; font-size: 14px;color: #221e1b; font-family: Arial, Helvetica, sans-serif; text-align:left;"><?php print var_dump($params);?></p>
 
 <div class="quote" style='padding: 80px 0 0 0; color: #796155; font-family:Georgia, "Times New Roman", Times, serif; font-style:italic; font-size: 13px; line-height: 20px;'>
  "FadMashion.com has changed the way you'll shop for independent fashion from now on."
- <img src="http://www.owenlinton.com/fadmashion/dev/email2/myfashionlife.jpg" alt="-myfashionlife" align="right" style="display:block;margin-top:14px;border:none;">
+ <img src="<?php print $theme_url?>/images/myfashionlife.jpg" alt="-myfashionlife" align="right" style="display:block;margin-top:14px;border:none;">
 </div>
 
 
@@ -33,10 +33,10 @@
 </div>
 </td>
 
-<td width="237"><img src="http://www.owenlinton.com/fadmashion/dev/email2/model.jpg" style="border: none; display: block;"></td>
+<td width="237"><img src="<?php print $theme_url?>/images/template2/model.jpg" style="border: none; display: block;"></td>
 </tr>
 <tr>
-<td colspan="2"><img src="http://www.owenlinton.com/fadmashion/dev/email2/footer.jpg" style="border: none; display: block;"></td>
+<td colspan="2"><img src="h<?php print $theme_url?>/images/template2/footer.jpg" style="border: none; display: block;"></td>
 
 </tr>
 </table>
