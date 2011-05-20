9a618eaaf91faa95b7faca83398e8a3158c96c4e
Anish Aggarwal <misternyce@gmail.com>
2011-05-20 11:49:12 -0400

    add template images

--- themes/fadmashion_commerce/htmlmail--fm_invite--confirmation.tpl.php
+++ themes/fadmashion_commerce/htmlmail--fm_invite--confirmation.tpl.php
@@ -1,7 +1,7 @@
-<div class="wrapper" align="center" style="background-color: #221e1b; padding: 20px 0;">
+<div class="wrapper" align="center" style="height: 100%; background-color: #221e1b; padding: 20px 0;">
 <table cellpadding="0" cellspacing="0" width="600" style="margin: 0 auto;">
 <tr>
-<td colspan="2"><img src="http://www.owenlinton.com/fadmashion/dev/email1b/header.jpg" style="border: none; display: block;"></td>
+<td colspan="2"><img src="<?php print $theme_url?>/images/emails/template1/header.jpg" style="border: none; display: block;"></td>
 </tr>
 <tr>
 <td class="content" valign="top" style="background-color: #efeee9;">
@@ -17,13 +17,13 @@
 
 
 <table cellpadding="0" cellspacing="0" class="rewardFrame">
-<tr><td><img src="http://www.owenlinton.com/fadmashion/dev/email1b/edge_reward_top.jpg" style="border: none; display: block;"></td></tr>
+<tr><td><img src="<?php print $theme_url?>/images/emails/template1/edge_reward_top.jpg" style="border: none; display: block;"></td></tr>
 
 <td style ="background:none repeat scroll 0 0 #FFFFFF; border-left:1px solid #D4C5B9; border-right:1px solid #D4C5B9; padding-left:4px;" class="rewardContent">
   	<div style="padding: 0 15px 22px 15px;" class="social">
     <h1 style="text-align:left;padding:10px 0;font-weight:normal;font-size:18px;color:#221e1b;font-family:Arial, Helvetica, sans-serif;width:auto;margin:4px 0;">Your Social Shopping Rewards Link</h1>
-<a target="_blank" href="https://www.facebook.com/dialog/feed?api_key=226095400738750&app_id=213872101957329&description=Fadmashion%20offers%20Independent%20fashion%20designs%20at%20up%20to%20%2560%20off%20retail%20price%20with%20fresh%20new%20deals%20every%20hour!%20%20Request%20an%20invitation%20today%20and%20get%20your%20own%20social%20rewards%20link%20to%20invite%20friends%20and%20earn%20priority%20access&display=popup&link=<?php print urlencode($body);?>&locale=en_US&message=Just%20joined%20FadMashion%20for%20up%20to%2060%25%20off%20Independent%20Designs!%20Check%20it%20out%20and%20share%20your%20own%20link%20so%20we%20can%20both%20earn%20Social%20Shopping%20Rewards.&name=Fadmashion%20Priority%20Access&next=http%3A%2F%2Fstatic.ak.fbcdn.net%2Fconnect%2Fxd_proxy.php%3Fversion%3D2%23cb%3Df3e646dee2d06ca%26origin%3Dhttp%253A%252F%252Fexp.fadmashion.com%252Ff1efeb3eb23316a%26relation%3Dopener%26transport%3Dpostmessage%26frame%3Df2f2816e89a3ec2%26result%3D%2522xxRESULTTOKENxx%2522&picture=http%3A%2F%2Fwww.fadmashion.com%2Fsites%2Fall%2Fthemes%2Ffadmashion_commerce%2Fimages%2Ffb%2Ffb-sequin-closeup.png&sdk=joey" class="socialIcon" style="color: #01517e; text-decoration:none;"><img style="margin: 0 5px 0 0; display: inline; border: medium none;" src="http://www.owenlinton.com/fadmashion/dev/email1b/icon_facebook.jpg" align="absmiddle" style="border: none; display: block;"></a>
-<a target="_blank" href="http://twitter.com/home?status=<?php print fm_invite_twitter_text($body);?>" class="socialIcon" style="color: #01517e; text-decoration:none;"><img src="http://www.owenlinton.com/fadmashion/dev/email1b/icon_twitter.jpg" align="absmiddle" style="margin: 0 5px 0 0; display: inline; border: medium none;"></a>
+<a target="_blank" href="https://www.facebook.com/dialog/feed?api_key=226095400738750&app_id=213872101957329&description=Fadmashion%20offers%20Independent%20fashion%20designs%20at%20up%20to%20%2560%20off%20retail%20price%20with%20fresh%20new%20deals%20every%20hour!%20%20Request%20an%20invitation%20today%20and%20get%20your%20own%20social%20rewards%20link%20to%20invite%20friends%20and%20earn%20priority%20access&display=popup&link=<?php print urlencode($body);?>&locale=en_US&message=Just%20joined%20FadMashion%20for%20up%20to%2060%25%20off%20Independent%20Designs!%20Check%20it%20out%20and%20share%20your%20own%20link%20so%20we%20can%20both%20earn%20Social%20Shopping%20Rewards.&name=Fadmashion%20Priority%20Access&next=http%3A%2F%2Fstatic.ak.fbcdn.net%2Fconnect%2Fxd_proxy.php%3Fversion%3D2%23cb%3Df3e646dee2d06ca%26origin%3Dhttp%253A%252F%252Fexp.fadmashion.com%252Ff1efeb3eb23316a%26relation%3Dopener%26transport%3Dpostmessage%26frame%3Df2f2816e89a3ec2%26result%3D%2522xxRESULTTOKENxx%2522&picture=http%3A%2F%2Fwww.fadmashion.com%2Fsites%2Fall%2Fthemes%2Ffadmashion_commerce%2Fimages%2Ffb%2Ffb-sequin-closeup.png&sdk=joey" class="socialIcon" style="color: #01517e; text-decoration:none;"><img style="margin: 0 5px 0 0; display: inline; border: medium none;" src="<?php print $theme_url?>/images/icon_facebook_brown.png" align="absmiddle" style="border: none; display: block;"></a>
+<a target="_blank" href="http://twitter.com/home?status=<?php print fm_invite_twitter_text($body);?>" class="socialIcon" style="color: #01517e; text-decoration:none;"><img src="<?php print $theme_url?>/images/icon_twitter_brown.png" align="absmiddle" style="margin: 0 5px 0 0; display: inline; border: medium none;"></a>
 <input type="text" value="<?php print $body; ?>" id="share_url" onclick="this.select()" class="form-text-readonly" autocomplete="off" style="width: 240px; border: 1px solid #c5bdb6; color: #72655c;  margin: 0 0 0 5px; padding: 2px 0 0 2px ;height: 22px;" name="share_url" readonly>
 </div>
 
@@ -34,40 +34,40 @@
     
     <table cellpadding="0" cellspacing="0"><tr>
 <td class="signup5" style="padding-right:5px;width:124px;">
-        <img src="http://www.owenlinton.com/fadmashion/dev/email1b/edge_5_top.jpg" style="border: none; display: block;"><div class="text" style="text-align:center;color:#fff;background:#7d6e63;">
+        <img src="<?php print $theme_url?>/images/emails/template1/edge_5_top.jpg" style="border: none; display: block;"><div class="text" style="text-align:center;color:#fff;background:#7d6e63;">
 <h1 style="color:#fff;font-family:Arial, Helvetica, sans-serif;text-align:center;width:auto;font-size:36px;margin:0px 0 4px 0;font-weight:normal;padding-top:5px;">5</h1>
 <h2 style="font-size: 13px; line-height: 13px; border-bottom: 1px solid #948d86; margin: 0 26px 16px 26px; padding-bottom: 12px; color: #fff; font-style:normal;">SIGN-UPS</h2>
 <p style="color:#fff;font-family:Arial, Helvetica, sans-serif;text-align:center;margin:0;font-size:13px;line-height:16px;padding:0 7px;">Priority Access and Free shipping for first purchase</p>
 <p class="value" style="color:#d7cbc2;font-family:Arial, Helvetica, sans-serif;text-align:center;margin:0;font-size:13px;line-height:16px;padding:14px 0 5px 0;">A $10 value</p>
 </div>
-<img src="http://www.owenlinton.com/fadmashion/dev/email1b/edge_5_bottom.jpg" style="border: none; display: block;">
+<img src="<?php print $theme_url?>/images/emails/template1/edge_5_bottom.jpg" style="border: none; display: block;">
 </td>
       
   
-  <td class="signup15" style="padding-right:5px;width:124px;">        <img src="http://www.owenlinton.com/fadmashion/dev/email1b/edge_15_top.jpg" style="border: none; display: block;"><div class="text" style="text-align:center;color:#fff;background:#605045;">
+  <td class="signup15" style="padding-right:5px;width:124px;">        <img src="<?php print $theme_url?>/images/emails/template1/edge_15_top.jpg" style="border: none; display: block;"><div class="text" style="text-align:center;color:#fff;background:#605045;">
 <h1 style="color:#fff;font-family:Arial, Helvetica, sans-serif;text-align:center;width:auto;font-size:36px;margin:0px 0 4px 0;font-weight:normal;padding-top:5px;">15</h1>
 
 <h2 style="font-size: 13px; line-height: 13px; border-bottom: 1px solid #948d86; margin: 0 26px 16px 26px; padding-bottom: 12px; color: #fff; font-style:normal;">SIGN-UPS</h2>
 <p style="color:#fff;font-family:Arial, Helvetica, sans-serif;text-align:center;margin:0;font-size:13px;line-height:16px;padding:0 7px;">Priority Access and Free shipping for 10 purchases</p>
 <p class="value" style="color:#d7cbc2;font-family:Arial, Helvetica, sans-serif;text-align:center;margin:0;font-size:13px;line-height:16px;padding:14px 0 5px 0;">A $100 value</p>
 </div>
-<img src="http://www.owenlinton.com/fadmashion/dev/email1b/edge_15_bottom.jpg" style="border: none; display: block;">
+<img src="<?php print $theme_url?>/images/emails/template1/edge_15_bottom.jpg" style="border: none; display: block;">
 </td>
    
    
-    <td class="signup25" style="width: 124px;">        <img src="http://www.owenlinton.com/fadmashion/dev/email1b/edge_25_top.jpg" style="border: none; display: block;"><div class="text" style="text-align:center;color:#fff;background:#473b2f;">
+    <td class="signup25" style="width: 124px;">        <img src="<?php print $theme_url?>/images/emails/template1/edge_25_top.jpg" style="border: none; display: block;"><div class="text" style="text-align:center;color:#fff;background:#473b2f;">
 <h1 style="color:#fff;font-family:Arial, Helvetica, sans-serif;text-align:center;width:auto;font-size:36px;margin:0px 0 4px 0;font-weight:normal;padding-top:5px;">25</h1>
 <h2 style="font-size: 13px; line-height: 13px; border-bottom: 1px solid #948d86; margin: 0 26px 16px 26px; padding-bottom: 12px; color: #fff; font-style:normal;">SIGN-UPS</h2>
 <p style="color:#fff;font-family:Arial, Helvetica, sans-serif;text-align:center;margin:0;font-size:13px;line-height:16px;padding:0 7px;">Priority Access and Free shipping for 20 purchases</p>
 <p class="value" style="color:#d7cbc2;font-family:Arial, Helvetica, sans-serif;text-align:center;margin:0;font-size:13px;line-height:16px;padding:14px 0 5px 0;">A $200 value</p>
 </div>
-<img src="http://www.owenlinton.com/fadmashion/dev/email1b/edge_25_bottom.jpg" style="border: none; display: block;">
+<img src="<?php print $theme_url?>/images/emails/template1/edge_25_bottom.jpg" style="border: none; display: block;">
 </td>
     </tr></table>
 </div>
    </td>
    
-    <tr><td><img src="http://www.owenlinton.com/fadmashion/dev/email1b/edge_reward_bottom.jpg" style="border: none; display: block;"></td></tr>
+    <tr><td><img src="<?php print $theme_url?>/images/emails/template1/edge_reward_bottom.jpg" style="border: none; display: block;"></td></tr>
 </table>
 <div style="padding:28px 0 0;text-align:center;font-family:Georgia,Times,serif;font-size:13px; font-style:italic;" class="blog">Stay inspired by following <a href="http://blog.fadmashion.com/" style="font-family:Georgia,Times,serif; font-size:13px; font-style:italic; color: #01517e; text-decoration:none;">Fadmashion's blog</a>
 </div>
@@ -77,8 +77,8 @@
 
 </td>
 
-<td width="165"><img src="http://www.owenlinton.com/fadmashion/dev/email1b/model.jpg" style="border: none; display: block;"></td>
+<td width="165"><img src="<?php print $theme_url?>/images/emails/template1/model.jpg" style="border: none; display: block;"></td>
 </tr>
-<tr><td colspan="2"><img src="http://www.owenlinton.com/fadmashion/dev/email1b/footer.jpg" style="border: none; display: block;"></td></tr>
+<tr><td colspan="2"><img src="<?php print $theme_url?>/images/emails/template1/footer.jpg" style="border: none; display: block;"></td></tr>
 </table>
 </div>
