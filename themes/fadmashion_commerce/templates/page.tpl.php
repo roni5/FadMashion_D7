c9ceea405b075ac734d00b3541e4db171849627f
Anish Aggarwal <misternyce@gmail.com>
2011-05-30 11:31:40 -0400

    add images

--- themes/fadmashion_commerce/templates/page.tpl.php
+++ themes/fadmashion_commerce/templates/page.tpl.php
@@ -15,15 +15,15 @@
             </div>
             <div class="row2"> 
 	        	    <a class="logo" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" >
-        			    <img src="<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/logo_fadmashion.png" alt="<?php print t('Home'); ?>" />
+        			    <img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/logo_fadmashion.png" alt="<?php print t('Home'); ?>" />
       			    </a>
-              <div class="navBox"><img src="<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/bg_nav_left.png">
+              <div class="navBox"><img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/bg_nav_left.png">
                     <ul class="nav">
                         <li class="active">30-Minute Deals</li>      
                         <li>Shop</li>         
                         <li>Socialize</li>     
                         <li>Rewards</li>
-                        </ul><img src="<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/bg_nav_right.png"></div>
+                        </ul><img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/bg_nav_right.png"></div>
             </div>
 	 </div><!--header-->
 	 <div id="content">
