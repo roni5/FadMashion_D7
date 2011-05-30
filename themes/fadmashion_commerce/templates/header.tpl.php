3eeed288a9a5bc5bbe7adeee7d406f7c6768ef22
Anish Aggarwal <misternyce@gmail.com>
2011-05-30 13:05:49 -0400

    add header files

---
+++ themes/fadmashion_commerce/templates/header.tpl.php
@@ -0,0 +1,24 @@
+<div id="header">
+        	<div class="row1">
+            	<div id="accountNav">
+                <ul class="tree">
+                <li class="menu"><a href="#">Anish</a></li>
+                <li class="menu"><a href="#">My Cart</a></li>
+                <li class="menu"><a href="#"><img class="flag" src="images/icon_flag_usa.png">$ USD</a></li>
+                <li><a href="#">Help</a></li>
+               </ul> </div>
+                <p>Lorem Ipsum dolor sit amet. <a class="learnMore" href="#">Learn More</a></p>
+            </div>
+            <div class="row2"> 
+	        	    <a class="logo" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" >
+        			    <img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/logo_fadmashion.png" alt="<?php print t('Home'); ?>" />
+      			    </a>
+              <div class="navBox"><img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/bg_nav_left.png">
+                    <ul class="nav">
+                        <li class="active">30-Minute Deals</li>      
+                        <li>Shop</li>         
+                        <li>Socialize</li>     
+                        <li>Rewards</li>
+                        </ul><img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/bg_nav_right.png"></div>
+            </div>
+	 </div><!--header-->
