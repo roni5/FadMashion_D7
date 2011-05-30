c8c08e03e2e3c539af5eb160fec21c7050bc2a6d
Anish Aggarwal <misternyce@gmail.com>
2011-05-29 22:30:07 -0400

    add header for entire site

---
+++ themes/fadmashion_commerce/templates/page.tpl.php
@@ -0,0 +1,28 @@
+
+<div align="center" id="wrapper">
+	<div class="container">
+
+        <div id="header">
+        	<div class="row1">
+            	<div id="accountNav">
+                <ul class="tree">
+                <li class="menu"><a href="#">Anish</a></li>
+                <li class="menu"><a href="#">My Cart</a></li>
+                <li class="menu"><a href="#"><img class="flag" src="images/icon_flag_usa.png">$ USD</a></li>
+                <li><a href="#">Help</a></li>
+               </ul> </div>
+                <p>Lorem Ipsum dolor sit amet. <a class="learnMore" href="#">Learn More</a></p>
+            </div>
+            <div class="row2"><a class="logo" href="#"><img alt="FadMashion" src="images/logo_fadmashion.png"></a>
+                <div class="navBox"><img src="images/bg_nav_left.png">
+                    <ul class="nav">
+                        <li class="active">30-Minute Deals</li>      
+                        <li>Shop</li>         
+                        <li>Socialize</li>     
+                        <li>Rewards</li>
+                        </ul><img src="images/bg_nav_right.png"></div>
+            </div>
+	 </div><!--header-->
+
+</div><!--wrapper-->
+</div><!--container-->
