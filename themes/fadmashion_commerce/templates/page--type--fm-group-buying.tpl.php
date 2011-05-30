2e75cb010fcb1709e1d088404b203583c59623de
Anish Aggarwal <misternyce@gmail.com>
2011-05-30 13:19:33 -0400

    add header to group buying template

--- themes/fadmashion_commerce/templates/page--type--fm-group-buying.tpl.php
+++ themes/fadmashion_commerce/templates/page--type--fm-group-buying.tpl.php
@@ -1,2 +1,9 @@
-<?php
-print 'test';
+<div align="center" id="wrapper">
+	<div class="container">
+<?php include('header.tpl.php');?>
+	 <div id="content">
+	    <?php print render($page['content']); ?>
+	 </div>
+
+</div><!--wrapper-->
+</div><!--container-->
