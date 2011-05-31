0a6560faef0d52c2209cd7fae81ac3f1badee7c9
Anish Aggarwal <misternyce@gmail.com>
2011-05-30 23:43:47 -0400

    done

--- modules/custom/fm_image_slider/fm_image_slider.js
+++ modules/custom/fm_image_slider/fm_image_slider.js
@@ -7,6 +7,7 @@ jQuery(document).ready(function() {
 	jQuery('#slider').nivoSlider({
         effect:'fade', // Specify sets like: 'fold,fade,sliceDown'
         startSlide:0, // Set starting Slide (0 index)
+        directionNav:false, // Next & Prev navigation
         controlNav:true, // 1,2,3... navigation
         controlNavThumbs:true, // Use thumbnails for Control Nav
         controlNavThumbsFromRel:true, // Use image rel for thumbs
