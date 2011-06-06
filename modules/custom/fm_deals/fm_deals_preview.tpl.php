5bfd266819b139d2ca59f49ced7d13b05a0fa00a
Anish Aggarwal <misternyce@gmail.com>
2011-06-06 01:01:48 -0400

    add stuff

--- modules/custom/fm_deals/fm_deals_preview.tpl.php
+++ modules/custom/fm_deals/fm_deals_preview.tpl.php
@@ -1,4 +1,4 @@
 <h1 class="itemName"><?php print $node->title; ?></h1>
 
 <a href="#"><?php print $image;?></a>
-<a href="#" class="button">Preview Upcoming Deals</a>
+<div class="button"><a href="#" >Preview Upcoming Deals</a></div>
