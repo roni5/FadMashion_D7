28c8906f2306362f7ee0b6ebf72771296f845229
Anish Aggarwal <misternyce@gmail.com>
2011-06-12 18:28:13 -0400

    shipping and returns policy

---
+++ modules/custom/fm_deals/fm_deals_shipping_returns.tpl.php
@@ -0,0 +1,5 @@
+<h2>$9.99 Shipping Anywhere in the US</h2>
+
+<h2>Return Policy</h2>
+<p>Due to the nature of our Deal offers, currently there is a no return policy.  
+If you have an issue or would like a different size please contact us directly and one of our customer support team will contact you </p>
