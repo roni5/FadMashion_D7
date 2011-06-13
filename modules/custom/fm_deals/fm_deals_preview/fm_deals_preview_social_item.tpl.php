4f6928439e5dcba0c89daa75a2a9a0ac36b9ed4f
Anish Aggarwal <misternyce@gmail.com>
2011-06-12 21:24:26 -0400

    add extra frame

---
+++ modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_social_item.tpl.php
@@ -0,0 +1,6 @@
+<div class="dealFrame">
+        <div class="social"><img src="images/icon_social.jpg">
+        <h1>Be the first to know!</h1>
+        Follow <a href="#">@Fadmashion</a> for deal updates or <a href="#">friend</a> us</div>
+        <div class="weekButton"><a class="" href="#">Preview Next Week's Deals</a></div>
+    </div>
