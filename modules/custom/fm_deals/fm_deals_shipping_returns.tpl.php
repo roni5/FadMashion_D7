e68b1e517e15ce1474fc72d87926b3e288de5da8
Anish Aggarwal <misternyce@gmail.com>
2011-06-13 12:40:57 -0400

    change shipping

--- modules/custom/fm_deals/fm_deals_shipping_returns.tpl.php
+++ modules/custom/fm_deals/fm_deals_shipping_returns.tpl.php
@@ -1,6 +1,7 @@
 
 <?php if ($isShipping) {?>
 <h2><b>$9.99</b> Shipping Anywhere in the US</h2>
+<p>Shipping Time: <b>4-5 weeks</b></p>
 <?php } else {?>
 <p>Due to the nature of our Deal offers, currently there is a no return policy.  
 If you have an issue or would like a different size please contact us directly and one of our customer support team will contact you </p>
