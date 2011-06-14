9300c2f4c7b65e75c7ba03cfe6319d2fbc2b9757
Anish Aggarwal <misternyce@gmail.com>
2011-06-14 11:19:11 -0400

    fix

--- themes/fadmashion_commerce/templates/page--intro.tpl.php
+++ themes/fadmashion_commerce/templates/page--intro.tpl.php
@@ -26,7 +26,7 @@ jQuery(function($){
 											{//Slide 2
 												image : '/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/front-page/splash_photo.jpg',
                         title: 'Designer: <a target="_blank" href="http://www.jonathansimkhai.com/jonathan.html" style="font-style: normal; font-family:\'museo-sans-1\',\'museo-sans-2\',Helvetica;" >Jonathan Simkhai</a>'
-											}
+											},
 											{//Slide 3
 												image : '/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/front-page/de_lingerie.jpg',
                         title: 'Designer: <a target="_blank" href="http://www.jonathansimkhai.com/jonathan.html?width=600&height=400&iframe=true" style="font-style: normal; font-family:\'museo-sans-1\',\'museo-sans-2\',Helvetica;" >Daisy & Elizabeth</a>'
