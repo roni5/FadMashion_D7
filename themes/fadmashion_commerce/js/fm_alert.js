b05ea40b74cf0c2e52557c24445e7f405eef0059
Anish Aggarwal <misternyce@gmail.com>
2011-06-14 15:04:34 -0400

    add alert box to notify pae

---
+++ themes/fadmashion_commerce/js/fm_alert.js
@@ -0,0 +1,23 @@
+/**
+ * fm_alert.js - Provides functioality for the Alert box at top of 
+ */
+
+/**
+ *  Javascript Tools to handle Forms in FAdmashion 
+ */
+
+jQuery(document).ready(function() {
+	
+	jQuery("a.alert").click(function (){
+		jQuery("a.alert").jAlert('Processing Request...', "warning", 'warningboxid');
+		var href_val = this.attr('href');
+		jQuery.ajax({
+			type: "POST", 
+			url: href_val 
+			complete: function(data){  
+			       alert(data);
+			}  
+		});
+	});
+
+});
