c3c9ea963c272e57c035a6b8382ee339fe9292d1
Anish Aggarwal <misternyce@gmail.com>
2011-06-14 11:23:59 -0400

    supersized longer transitions

--- themes/fadmashion_commerce/templates/page--intro.tpl.php
+++ themes/fadmashion_commerce/templates/page--intro.tpl.php
@@ -13,6 +13,8 @@ jQuery(function($){
 	$.supersized({
 		start_slide				:	0,
 		vertical_center   : 0,
+		slide_interval : 10000,
+		transition_speed : 1200,
 		slideshow : 1, 
 		navigation              :   1,		//Slideshow controls on/off
 		thumbnail_navigation    :   1,		//Thumbnail navigation
