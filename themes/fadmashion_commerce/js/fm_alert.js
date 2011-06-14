2502ec478d9fc99a2a88d6ba536cfece9a67bbc5
Anish Aggarwal <misternyce@gmail.com>
2011-06-14 17:46:06 -0400

    alertbox logic

--- themes/fadmashion_commerce/js/fm_alert.js
+++ themes/fadmashion_commerce/js/fm_alert.js
@@ -17,7 +17,7 @@ jQuery(document).ready(function() {
 			url: href_val ,
 			complete: function(data){  
 				jQuery(".msg-text").html(data.responseText);
-				alignCenter();
+				fm_alignAlertCenter(jQuery("body"));
 				timeout = setTimeout('fm_clearAlertBox()', "4000");
 			}  
 		});
@@ -32,4 +32,17 @@ function fm_clearAlertBox() {
 			jQuery(this).remove();
 		});
 	}	
+}
+
+function fm_alignAlertCenter(containterObj) {
+	var alert_box_width = jQuery(".msg-box-cont").width();
+    //get the width of the container
+    var container_width = containterObj.innerWidth();
+    // get the x position of the container
+    var container_left = containterObj.x();
+    //get the center position of the alert box within the container
+    var actual_left = ((container_width-alert_box_width)/2)+container_left;
+    //get the y (top) position of the container
+    jQuery(".msg-box-cont").css("left",actual_left+"px");
+	
 }
