b5162ef5b8ec04b8f333c9292ec498970d5bd4dd
Anish Aggarwal <misternyce@gmail.com>
2011-06-15 15:40:10 -0400

    wewer

--- modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_item.tpl.php
+++ modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_item.tpl.php
@@ -5,7 +5,7 @@ $unixStartTime = strtotime($start_time);
 ?>
 
 <div class="dealFrame <?php print ($node->deal_status == 'active' ? 'onSale' : '');?>">
-  <div class="dealHeader <?php print ($node->deal_status == 'ended' ? 'ended' : '');?>"><?php ($node->deal_status == 'upcoming' ? print t('Deal Starts @') . date(" ga T", strtotime($node->field_start_time['und'][0]['value'])) : ($node->deal_status == 'active' ? print t('On Sale Now') : print t('This sale has ended'))) ?></div>
+  <div class="dealHeader <?php print ($node->deal_status == 'ended' ? 'ended' : '');?>"><?php ($node->deal_status == 'upcoming' ? print t('Starts @') . '<b>' . date(" ga T", strtotime($node->field_start_time['und'][0]['value'])) . '</b>' : ($node->deal_status == 'active' ? print t('On Sale Now') : print t('This sale has ended'))) ?></div>
     <div class="pricing">
       <div class="original">Original<br>Price<h3><?php print render($node->content['product:commerce_price']); ?></h3></div>
       <div class="exclusive">Exclusive<br>Deal<h3><?php print render($node->content['field_sale_price']); ?></h3></div>
@@ -19,7 +19,6 @@ $unixStartTime = strtotime($start_time);
     <div class="button"><?php print l('View Now', 'node/' . $node->nid );?></div>
     <?php } else if($node->deal_status == 'upcoming') { ?>
     <div class="button"><?php print l('Preview', 'node/' . $node->nid );?></div>
-    <div class="button"><a href="#">Remind Me</a></div>
      <?php }  ?>
      </div>
  </div>
--- modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_week.tpl.php
+++ modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_week.tpl.php
@@ -1,5 +1,5 @@
 <div id="dealPreview">
-<h1>Preview of Monday's Exclusive Group Deals</h1>
+<h1>This Monday's Exclusive Group Deals</h1>
 <h2><?php print date("F d, Y", strtotime($date)) ?></h2>
 <?php 
 foreach($items as $item) {
