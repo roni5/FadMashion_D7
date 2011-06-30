613f1b766a2f844801e7d74204972af5c296b237
Anish Aggarwal <misternyce@gmail.com>
2011-06-30 19:56:44 -0400

    change title

--- modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_block.tpl.php
+++ modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_block.tpl.php
@@ -1,4 +1,4 @@
-<h1 class="itemName"><?php print $node->title; ?></h1>
+<h1 class="itemName"><?php print $product->title; ?></h1>
 <h3 style="padding-bottom: 10px"> <?php print l($store->name, 'test'); ?></h3>
 
 <a href="#"><?php print l($image, 'node/'.$node->nid, array('html' => true));?></a>
