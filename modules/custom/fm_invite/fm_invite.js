4537b2374f7ddf58acb95f4b1f058b69e59b8f08
Anish Aggarwal <misternyce@gmail.com>
2011-07-08 12:17:40 -0400

    asdf

--- modules/custom/fm_invite/fm_invite.js
+++ modules/custom/fm_invite/fm_invite.js
@@ -33,7 +33,7 @@ jQuery(document).ready(function() {
       expires.setDate(expires.getDate()+30);
       document.cookie = "visited=true; expires="+expires.toUTCString();
  
-      jQuery.colorbox({href:"/invite/rewards?id=195", width: 518, height: 326});
+      jQuery.colorbox({title:'Invite Friends and Earn Rewards', href:"/invite/rewards?id=195", width: 518, height: 326});
   }
 });
 
