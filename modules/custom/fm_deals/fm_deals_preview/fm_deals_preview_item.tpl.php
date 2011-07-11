fa3685d9a6ddbe7fafb1c1e60524db99aa731470
Anish Aggarwal <misternyce@gmail.com>
2011-07-11 10:27:13 -0400

    put in block

--- modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_item.tpl.php
+++ modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_item.tpl.php
@@ -6,14 +6,14 @@ $unixStartTime = strtotime($start_time);
 
 <div class="dealFrame <?php print ($node->deal_status == 'active' ? 'onSale' : '');?>">
   <div class="dealHeader <?php print ($node->deal_status == 'ended' ? 'ended' : '');?>"><?php ($node->deal_status == 'upcoming' ? print t('Starts @') . '<b>' . date(" ga T", strtotime($node->field_start_time['und'][0]['value'])) . '</b>' : ($node->deal_status == 'active' ? print t('On Sale Now') : print t('This sale has ended'))) ?></div>
+    <h1 class="itemName"><?php print $node->title; ?></h1>
+    <h2>by <?php print l($store->name, 'test'); ?></h2>
+    <?php print l($image, 'node/' . $node->nid, array('html' => true)); ?>
     <div class="pricing">
       <div class="original">Original<br>Price<h3><?php print render($node->content['product:commerce_price']); ?></h3></div>
       <div class="exclusive">Exclusive<br>Deal<h3><?php print render($node->content['field_sale_price']); ?></h3></div>
       <div class="savings">Savings<h3><?php print $node->sale_percentage; ?></h3></div>
     </div><!--pricing--> 
-    <?php print l($image, 'node/' . $node->nid, array('html' => true)); ?>
-    <h1 class="itemName"><?php print $node->title; ?></h1>
-    <h2>by <?php print l($store->name, 'test'); ?></h2>
     <div class="buttons">
     <?php if($node->deal_status == 'active') { ?>
     <div class="button"><?php print l('View Now', 'node/' . $node->nid );?></div>
