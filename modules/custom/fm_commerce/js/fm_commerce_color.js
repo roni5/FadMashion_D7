1eef509f59d60d7ef3af0659b79e637c6a410500
Anish Aggarwal <misternyce@gmail.com>
2011-07-18 15:25:03 -0400

    push changes

--- modules/custom/fm_commerce/js/fm_commerce_color.js
+++ modules/custom/fm_commerce/js/fm_commerce_color.js
@@ -33,21 +33,37 @@ jQuery(document).ready(function() {
             }
         });
      }
-	    
-	    
+	 
+
+		//Enable Javascript Color functionality for Fadmashion sizes
+		jQuery('.form-item-sizes select').selectBox();
+		
 	//Enable Javascript Color functionality for Fadmashion Commerce
-	jQuery('.form-item-colors select').selectBox();
+		jQuery('.form-item-colors select').selectBox();
+		
+		//Change color options to div structures
+		jQuery('.form-item-colors .selectBox-options li a').each(function() {
+			var vals = jQuery(this).attr('rel').split('_');
+			var code = vals[0];
+			var title = vals[1];
+			var val = jQuery(this).html();
+			jQuery(this).attr('style', 'background-color: #' + code);
+			jQuery(this).attr('title', title);
+			jQuery(this).attr('pos', val);
+			jQuery(this).html('');
+		});
+		
+		
+		jQuery('.form-item-colors li a').click(function() {
+			var title = jQuery('.form-item-colors .selectBox-selected a').attr('title');
+			jQuery('.form-item-colors .selectBox-options .colorName').html(title);
+		});
+		
+		var title = jQuery('.form-item-colors .selectBox-selected a').attr('title');
+		jQuery('.form-item-colors .selectBox-options').before('<div class="colorName">' + title + '</div>');
+		
 	
-	//Change color options to div structures
-	jQuery('.form-item-colors .selectBox-options li a').each(function() {
-		var code = jQuery(this).attr('rel');
-		var val = jQuery(this).html();
-		jQuery(this).attr('style', 'background-color: #' + code);
-		jQuery(this).attr('pos', val);
-		jQuery(this).html('');
-	});
-	
-	//Enable Javascript Color functionality for Fadmashion sizes
-	jQuery('.form-item-sizes select').selectBox();
+		
+		
 	
 });
