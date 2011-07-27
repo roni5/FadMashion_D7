0050fd8c9c10571412ea6023384db8bf85cf7f61
Anish Aggarwal <misternyce@gmail.com>
2011-07-27 14:00:57 -0400

    add hover overs

--- modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_block.tpl.php
+++ modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_block.tpl.php
@@ -1,5 +1,5 @@
 <h1 class="itemName"><?php print $product->title; ?></h1>
-<h3 style="padding-bottom: 10px"> <?php print l($store->name, 'test'); ?></h3>
+<h3 style="padding-bottom: 10px"> <?php print theme('fm_commerce_store_name', array('store' => $store)); ?></h3>
 
 <a href="#"><?php print l($image, 'node/'.$node->nid, array('html' => true));?></a>
-<?php print l('Preview More Deals', 'deals_preview', array('query' => array('width' => '910px'),  'attributes' => array('class' => array('colorbox-load', 'button'))) ); ?>
+<div id="upNext"><?php print l('Preview More Deals', 'deals_preview', array('query' => array('width' => '910px'),  'attributes' => array('class' => array('colorbox-load', 'button'))) ); ?></div>
--- modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_item.tpl.php
+++ modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_item.tpl.php
@@ -7,7 +7,7 @@ $unixStartTime = strtotime($start_time);
 <div class="dealFrame <?php print ($node->deal_status == 'active' ? 'onSale' : '');?>">
   <div class="dealHeader <?php print ($node->deal_status == 'ended' ? 'ended' : '');?>"><?php ($node->deal_status == 'upcoming' ? print t('Starts @') . '<b>' . date(" ga T", strtotime($node->field_start_time['und'][0]['value'])) . '</b>' : ($node->deal_status == 'active' ? print t('On Sale Now') : print t('This sale has ended'))) ?></div>
     <h1 class="itemName"><?php print $node->title; ?></h1>
-    <h2>by <?php print l($store->name, 'test'); ?></h2>
+    <h2>by <?php print theme('fm_commerce_store_name', array('store' => $store)); ?></h2>
     <?php print l($image, 'node/' . $node->nid, array('html' => true)); ?>
     <div class="pricing">
       <div class="original">Original<br>Price<h3><?php print render($node->content['product:commerce_price']); ?></h3></div>
@@ -16,9 +16,9 @@ $unixStartTime = strtotime($start_time);
     </div><!--pricing--> 
     <div class="buttons">
     <?php if($node->deal_status == 'active') { ?>
-    <div class="button"><?php print l('View Now', 'node/' . $node->nid );?></div>
+    <div><?php print l('View Now', 'node/' . $node->nid, array('attributes' => array('class' => array( 'button'))) );?></div>
     <?php } else if($node->deal_status == 'upcoming') { ?>
-    <div class="button"><?php print l('Preview', 'node/' . $node->nid );?></div>
+    <div ><?php print l('Preview', 'node/' . $node->nid, array('attributes' => array('class' => array( 'button'))) );?></div>
      <?php }  ?>
      </div>
  </div>
