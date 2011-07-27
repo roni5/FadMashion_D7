57c7b3e576995b71db63d91e772111f54054bd9a
Anish Aggarwal <misternyce@gmail.com>
2011-07-27 11:16:44 -0400

    make changes

--- modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_block.tpl.php
+++ modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_block.tpl.php
@@ -2,4 +2,4 @@
 <h3 style="padding-bottom: 10px"> <?php print l($store->name, 'test'); ?></h3>
 
 <a href="#"><?php print l($image, 'node/'.$node->nid, array('html' => true));?></a>
-<div class="button"><?php print l('Preview More Deals', 'deals_preview', array('query' => array('width' => '910px'),  'attributes' => array('class' => 'colorbox-load')) ); ?></div>
+<?php print l('Preview More Deals', 'deals_preview', array('query' => array('width' => '910px'),  'attributes' => array('class' => array('colorbox-load', 'button'))) ); ?>
