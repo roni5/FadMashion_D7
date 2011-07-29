5fa231b9d1b3b08eced788655a7037dd290fdc25
Anish Aggarwal <misternyce@gmail.com>
2011-07-29 15:31:19 -0400

    push changes

--- modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_week.tpl.php
+++ modules/custom/fm_deals/fm_deals_preview/fm_deals_preview_week.tpl.php
@@ -1,9 +1,12 @@
 <div id="dealPreview">
 <h1>Preview Monday's Exclusive Deals</h1>
 <h2><?php print date("F d, Y", strtotime($date)) ?></h2>
+<div id="dealPreview">
 <?php 
 foreach($items as $item) {
 	print $item;
 }
 ?>
+</div>
+
 </div>
