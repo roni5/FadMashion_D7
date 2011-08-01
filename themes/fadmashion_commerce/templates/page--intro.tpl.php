35c8a0092890fcc396307b0aaf9cd16cafae0614
Anish Aggarwal <misternyce@gmail.com>
2011-08-01 18:53:50 -0400

    push

--- themes/fadmashion_commerce/templates/page--intro.tpl.php
+++ themes/fadmashion_commerce/templates/page--intro.tpl.php
@@ -103,7 +103,7 @@ jQuery(function($){
 	</div>
 </div>
 
-<?php if(!empty($page['<?php endif;  ?>'])): ?>
+<?php if(!empty($page['footer'])): ?>
 <div id="footer">
 <?php print render($page['footer']); ?>
 </div>
