a8a407aa9c70e135c3eafd32be75d240d5ef7215
Anish Aggarwal <misternyce@gmail.com>
2011-08-03 01:02:47 -0400

    push changes

--- themes/fadmashion_commerce/templates/page.tpl.php
+++ themes/fadmashion_commerce/templates/page.tpl.php
@@ -1,13 +1,24 @@
-
 <div align="center" id="wrapper">
-	<div class="container">
-<?php include('header.tpl.php');?>
-	 <div id="content">
-	    <?php print render($page['content']); ?>
+	<div class="container" style="width: 680px;">
+<?php include('header-static.tpl.php');?>
+  <div id="static">
+	 <div id="content" class="sidebarRight" style="margin-top: 20px; background: none repeat scroll 0% 0% transparent;">
+	   <div class="column1">
+	     
+<?php if ($messages): ?>
+      <div id="messages"><div class="section clearfix">
+         <?php print $messages; ?>
+       </div></div> <!-- /.section, /#messages -->
+       <?php endif; ?>
+  
+	        <?php print render($page['content']); ?>
+	   </div> <!-- end column1 -->
+	   <div class="column2">
+	     <div class="sidebar">
+	       <?php //print render($page['sidebar_second']); ?>
+	     </div>
+	   </div>
 	 </div>
-
+</div>
 </div><!--wrapper-->
 </div><!--container-->
-
-<br clear="all">
-<?php include('footer.tpl.php');?>
