26d69c954e6db463621a2512dc5ed2819199ec0c
Anish Aggarwal <misternyce@gmail.com>
2011-08-23 23:04:47 -0400

    push changes

--- themes/fadmashion_commerce/templates/header-checkout.tpl.php
+++ themes/fadmashion_commerce/templates/header-checkout.tpl.php
@@ -2,7 +2,7 @@
   <div class="row2">
      <?php print l('Continue Shopping', '', array('attributes' => array('class' => array('button') ) ));?>
      <a class="logo" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" >
-         <img src="<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/logo_fadmashion.png" alt="<?php print t('fadmashion'); ?>" />
+         <img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/logo_fadmashion.png" alt="<?php print t('fadmashion'); ?>" />
       </a>
   <div class="title">Express Checkout</div>
   </div>
