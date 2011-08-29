05267541480c6fe93bef7e57820a879c8c57688f
Anish Aggarwal <misternyce@gmail.com>
2011-08-29 00:29:15 -0400

    push changes

--- modules/custom/fm_image_slider/fm_image_slider.js
+++ modules/custom/fm_image_slider/fm_image_slider.js
@@ -9,9 +9,9 @@ jQuery(document).ready(function() {
         startSlide:0, // Set starting Slide (0 index)
         directionNav:false, // Next & Prev navigation
         directionNavHide:true, // Only show on hover
-        controlNav:true, // 1,2,3... navigation
+        controlNav:showThumbs, // 1,2,3... navigation
         controlNavThumbs: showThumbs, // Use thumbnails for Control Nav
-        controlNavThumbsFromRel:true, // Use image rel for thumbs
+        controlNavThumbsFromRel:showThumbs, // Use image rel for thumbs
         keyboardNav:true, // Use left & right arrows
         manualAdvance:true,
         pauseTime:6000, 
