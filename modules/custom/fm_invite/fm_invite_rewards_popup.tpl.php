9374cc29274d67baac3703e5046e602874b27925
Anish Aggarwal <misternyce@gmail.com>
2011-08-28 21:11:34 -0400

    push

--- modules/custom/fm_invite/fm_invite_rewards_popup.tpl.php
+++ modules/custom/fm_invite/fm_invite_rewards_popup.tpl.php
@@ -3,7 +3,7 @@
 <div class="share">
 
 	<div class="social">
-  <a class="socialIcon" href="javascript:void(0);" onClick="javascript:fm_invite_facebookshare( 'http://www.fadmashion.com/<?php print fm_invite_get_invite_url();?>', '<?php print drupal_get_path("theme","fadmashion_commerce") . '/images/fb/fb-sequin-closeup.png'; ?>');"><img width="25" src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/icon_facebook_white.jpg"></a>
+  <a class="socialIcon" href="javascript:void(0);" onClick="javascript:fm_invite_facebookshare( '<?php print fm_invite_get_invite_url();?>', 'http://www.fadmashion.com/<?php print drupal_get_path("theme","fadmashion_commerce") . '/images/fb/fb-sequin-closeup.png'; ?>');"><img width="25" src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/icon_facebook_white.jpg"></a>
   <a class="socialIcon" target="_blank" href="http://twitter.com/intent/tweet?text=<?php print fm_invite_twitter_text()?>"><img width="25" src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/icon_twitter_white.jpg"></a>
   <?php 
     $image = '<img src="/' . drupal_get_path("theme","fadmashion_commerce") . '/images/icon_mail_white.jpg">';
