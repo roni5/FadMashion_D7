9d3dc65a56f247f1f64d8c5e28d458f4310786e0
Anish Aggarwal <misternyce@gmail.com>
2011-09-16 14:25:44 -0400

    push changes

--- themes/fadmashion_commerce/htmlmail.tpl.php
+++ themes/fadmashion_commerce/htmlmail.tpl.php
@@ -4,12 +4,13 @@
  * Sample template for HTML Mail test messages.
  */
 ?>
-<table cellspacing="0" cellpadding="0" class="main" style="margin-left: 50px; width: 500px; background: #fff;"><tbody><tr><td style="padding; 10px; background: #fff; border-top: 5px solid #bdb6af; border-bottom: 5px solid #bdb6af; text-align:left;">
-
-
-<div class="logo" style="padding: 0 0 12px 0; margin-top: 25px;"><a href="http://www.fadmashion.com" style='font-size: 20px; font-family: Georgia, "Times New Roman", Times, serif; font-style:italic;'><img style=" border: 0px solid #000;" title="fadmashion" src="<?php print $theme_url?>/logo_black.jpg"></a></div>
-
-<div style="margin: 20px 0;"><?php print $body;?></div>
-
-
-</td></tr></tbody></table>
+<table width="100%" cellspacing="0" cellpadding="0"><tbody>
+<tr>
+<td class="headerSide">&nbsp;</td>
+<td class="header" style="padding: 30px 0 10px 0;"><img alt="Fadmashion.com | Choose Independent Fashion" src="<?php print $theme_url;?>/images/logo_fadmashion.png"></td>
+</tr>
+<tr>
+<td class="headerSide">&nbsp;</td>
+</tr>
+<?php print $body;?>
+</tbody></table>
