7466299e86f29e6f3e546496ab9be32f36fc4979
Anish Aggarwal <misternyce@gmail.com>
2011-09-16 19:19:25 -0400

    push changes

--- modules/custom/fm_deals/fm_deals_notify/fm_deals_notify_mail.tpl.php
+++ modules/custom/fm_deals/fm_deals_notify/fm_deals_notify_mail.tpl.php
@@ -1,28 +1,15 @@
-<h1 style="font-weight: normal; font-size: 28px; margin-top: 0;">Savvy darling. Very savvy. </h1>
-<p style="margin-bottom: 10px; font-size: 14px; line-height: 18px; margin-top: 0;">If your deal is "on" you will be notified, and your designer will ship your item as described under the product description on their deal page. Thank you for shopping with Fadmashion, the best way to shop Independent Fashion online. </p>
+<td valign="top" class="content" style="width: 485px; background: #f3efeb; border-top: 3px solid #dfd7d0; border-bottom: 3px solid #dfd7d0; font-family:Arial, Helvetica, sans-serif; color: #3d3732; padding-top: 16px;">
+<div class="bar" style="background: #529e37; height: 36px; line-height: 36px; font-size: 18px; font-weight:bold; color: #fff; position: relative; top: -18px; padding-left: 8px;">
+<img align="absmiddle" src="bar_icon.jpg"> Your deal is on</div>
 
-    
-    <table cellspacing="0" cellpadding="10" class="message" style="margin: 10px 0 25px 0; background: #ffffff; border: 1px solid #e2dcd6"><tbody>
-<tr>
-<td class="confirmation" colspan="2" style="vertical-align: top; background: #f8f5f2; border-bottom:1px solid #e2dcd6">Confirmation Number :   <b><?php print $order->order_id?></b>
-</td>
-        </tr>
-<tr>
-<td class="col1" style="width: 146px; vertical-align: top;"><a href="#" style="color: #1c7eb4;"><?php print $thumb;?></a></td>
-            <td class="col2" style="vertical-align: top;">
-            <div class="itemName" style="font-size: 16px; font-weight: bold; line-height: 20px; margin-bottom: 4px;"><?php print $product->title; ?></div>
-        <div class="designer" style="font-size: 12px; padding-bottom: 10px; border-bottom: 1px solid #e2dcd6;"><?php print $shop->name; ?></div>
-        <div class="shippingTime" style="font-size: 12px; padding: 10px 0; border-bottom: 1px solid #e2dcd6;">Shipping Time: 4-5 weeks</div>
-     <?php 
-$order_view = commerce_order_ui_order_view($order, 'customer');
-?>    
-        <div class="ships" style="color: #a4897b; font-size: 12px; font-weight: bold; margin-top: 10px;">SHIPS TO:</div>
-        <p style="margin-bottom: 10px; font-size: 14px; line-height: 18px; margin-top: 0;"><?php print render($order_view['commerce_customer_shipping']);  ?></p>
-        
-        
-            </td>
-        </tr>
-</tbody></table>
-<p style="margin-bottom: 10px; font-size: 14px; line-height: 18px; margin-top: 0;">Sign into your account on <a href="http://www.fadmashion.com" style="color: #1c7eb4;">Fadmashion</a> to review your purchase confirmation. Thanks for shopping with us and for supporting Independent Fashion!</p>
+	<table cellspacing="0" cellpadding="0" class="message" style="margin: 10px 0 25px 0; background: #ffffff; border: 1px solid #e2dcd6"><tbody><tr>
+<td class="col1" style="width: 146px; vertical-align: top;"><a href="#" style="color: #1c7eb4;"><img class="item" src="item.jpg" style="border: 1px solid #e2dcd6; width: 142px;"><img src="button.jpg"></a></td>
+    <td class="col2" style="vertical-align: top;">
+    <div class="itemName" style="font-size: 16px; font-weight: bold; line-height: 20px; margin-bottom: 4px;">Charlie Foldover Messenger Bag Charlie Foldover Messenger Bag</div>
+<div class="designer" style="font-size: 12px; padding-bottom: 10px; border-bottom: 1px solid #e2dcd6;">by Cri de Coeur</div>
+<p style="margin-bottom: 10px; font-size: 14px; line-height: 18px; margin-top: 0;">This is a reminder that in 15 minutes, the deal you were interested in on Fadmashion will start. Check out the <a href="#" style="color: #1c7eb4;">preview page</a> and get ready to shop!</p>
+<p style="margin-bottom: 10px; font-size: 14px; line-height: 18px; margin-top: 0;">Enjoy!</p>
 <p class="signature" style="margin-bottom:10px;font-size:12px;line-height:18px;margin-top:0;font-style:italic;">- The Fadmashion Team</p>
-<br><br>
+</td>
+    </tr></tbody></table>
+</td>
