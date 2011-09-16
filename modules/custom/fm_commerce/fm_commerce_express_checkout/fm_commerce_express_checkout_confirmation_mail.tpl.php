7c29d05a077082c452eaccc02896db47a99e48ae
Anish Aggarwal <misternyce@gmail.com>
2011-09-16 18:04:32 -0400

    push changes

--- modules/custom/fm_commerce/fm_commerce_express_checkout/fm_commerce_express_checkout_confirmation_mail.tpl.php
+++ modules/custom/fm_commerce/fm_commerce_express_checkout/fm_commerce_express_checkout_confirmation_mail.tpl.php
@@ -4,26 +4,25 @@
     
     <table cellspacing="0" cellpadding="10" class="message" style="margin: 10px 0 25px 0; background: #ffffff; border: 1px solid #e2dcd6"><tbody>
 <tr>
-<td class="confirmation" colspan="2" style="vertical-align: top; background: #f8f5f2; border-bottom:1px solid #e2dcd6">Confirmation Number :   <b>A0000240380</b>
+<td class="confirmation" colspan="2" style="vertical-align: top; background: #f8f5f2; border-bottom:1px solid #e2dcd6">Confirmation Number :   <b><?php print $order->order_id?></b>
 </td>
         </tr>
 <tr>
-<td class="col1" style="width: 146px; vertical-align: top;"><a href="#" style="color: #1c7eb4;"><img class="item" src="item.jpg" style="border: 1px solid #e2dcd6; width: 142px;"></a></td>
+<td class="col1" style="width: 146px; vertical-align: top;"><a href="#" style="color: #1c7eb4;"><?php print $thumb;?></a></td>
             <td class="col2" style="vertical-align: top;">
-            <div class="itemName" style="font-size: 16px; font-weight: bold; line-height: 20px; margin-bottom: 4px;">Charlie Foldover Messer Bag Clie Foldover Messenger Bag</div>
-        <div class="designer" style="font-size: 12px; padding-bottom: 10px; border-bottom: 1px solid #e2dcd6;">by Cri de Coeur</div>
+            <div class="itemName" style="font-size: 16px; font-weight: bold; line-height: 20px; margin-bottom: 4px;"><?php print $product->title; ?></div>
+        <div class="designer" style="font-size: 12px; padding-bottom: 10px; border-bottom: 1px solid #e2dcd6;"><?php print $product->title; ?></div>
         <div class="shippingTime" style="font-size: 12px; padding: 10px 0; border-bottom: 1px solid #e2dcd6;">Shipping Time: 4-5 weeks</div>
-        
+     <?php 
+$order_view = commerce_order_ui_order_view($order, 'customer');
+?>    
         <div class="ships" style="color: #a4897b; font-size: 12px; font-weight: bold; margin-top: 10px;">SHIPS TO:</div>
-        <p style="margin-bottom: 10px; font-size: 14px; line-height: 18px; margin-top: 0;">Owen Linton<br>
-2310 E. Huntingdon St.<br>
-Philadelphia, PA 19125<br>
-USA</p>
+        <p style="margin-bottom: 10px; font-size: 14px; line-height: 18px; margin-top: 0;"><?php print render($order_view['commerce_customer_shipping']);  ?></p>
         
         
             </td>
         </tr>
 </tbody></table>
-<p style="margin-bottom: 10px; font-size: 14px; line-height: 18px; margin-top: 0;">Sign into your account on <a href="#" style="color: #1c7eb4;">Fadmashion</a> to review your purchase confirmation. Thanks for shopping with us and for supporting Independent Fashion!</p>
+<p style="margin-bottom: 10px; font-size: 14px; line-height: 18px; margin-top: 0;">Sign into your account on <a href="http://www.fadmashion.com" style="color: #1c7eb4;">Fadmashion</a> to review your purchase confirmation. Thanks for shopping with us and for supporting Independent Fashion!</p>
 <p class="signature" style="margin-bottom:10px;font-size:12px;line-height:18px;margin-top:0;font-style:italic;">- The Fadmashion Team</p>
 <br><br>
