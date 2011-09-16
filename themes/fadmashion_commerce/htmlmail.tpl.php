43bac26b28b875cc9e508f4472244b159f742f12
Anish Aggarwal <misternyce@gmail.com>
2011-09-16 14:35:28 -0400

    commit and push changes

--- themes/fadmashion_commerce/htmlmail.tpl.php
+++ themes/fadmashion_commerce/htmlmail.tpl.php
@@ -7,7 +7,7 @@
 <table width="100%" cellspacing="0" cellpadding="0"><tbody>
 <tr>
 <td class="headerSide">&nbsp;</td>
-<td class="header" style="padding: 30px 0 10px 0;"><img alt="Fadmashion.com | Choose Independent Fashion" src="<?php print $theme_url;?>/images/logo_fadmashion.png"></td>
+<td class="header" style="padding: 30px 0 10px 0;"><img alt="Fadmashion.com | Choose Independent Fashion" src="<?php print $theme_url;?>/images/logo_black.jpg"></td>
 </tr>
 <tr>
 <td class="headerSide">&nbsp;</td>
