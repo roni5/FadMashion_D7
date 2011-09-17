3d30cf36ef3d00f0daa21c627dfe4a9d9fe4230f
Anish Aggarwal <misternyce@gmail.com>
2011-09-17 17:30:04 -0400

    make changes

--- themes/fadmashion_commerce/templates/footer.tpl.php
+++ themes/fadmashion_commerce/templates/footer.tpl.php
@@ -2,10 +2,17 @@
 <div id="footer">
     <div class="container">
     <div class="box">
-    <?php //<div class="subCol"><h1>Have a question?</h1><a href="#">Contact us anytime</a></div> ?>
+    <div class="subCol"><h1>Have a question?</h1><a href="mailto:support@fadmashion.com">Contact us anytime</a></div> 
+    <div class="subCol"><h1>Share the deals</h1>
+    
+      <img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/icon_share_mail.jpg">
+      <img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/icon_share_facebook.jpg">
+      <img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/icon_share_twitter.jpg">
+    </div>
        
  
     </div>
+    
         <div class="links">   <a href="http://blog.fadmashion.com" >Blog</a><a href="mailto:info@fadmashion.com" >Contact Us</a><?php print l('FAQ', 'faq' ); ?></div>
         <div class="copyright">Copyright &copy; 2011, Fadmashion</div>
         <br clear="all">
