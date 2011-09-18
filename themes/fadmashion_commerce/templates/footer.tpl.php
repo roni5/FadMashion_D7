a3db243c0a4c77ee3c0b12396adfe0ed68b63a64
Anish Aggarwal <misternyce@gmail.com>
2011-09-18 16:41:48 -0400

    push changes

--- themes/fadmashion_commerce/templates/footer.tpl.php
+++ themes/fadmashion_commerce/templates/footer.tpl.php
@@ -11,7 +11,7 @@
     </div>
 </div>
     
-        <div class="links"><?php print l('Our Story');?><a href="http://blog.fadmashion.com" >Blog</a><a href="mailto:info@fadmashion.com" >Contact Us</a><?php print l('FAQ', 'faq' ); ?><?php print l('About Us', 'about');?></div>
+        <div class="links"><?php print l('Our Story', 'story');?><a href="http://blog.fadmashion.com" >Blog</a><a href="mailto:info@fadmashion.com" >Contact Us</a><?php print l('FAQ', 'faq' ); ?><?php print l('About Us', 'about');?></div>
         <div class="copyright">Copyright &copy; 2011, Fadmashion</div>
         <br clear="all">
     </div>
