f6b921cb8efa3f35ec75730eaf0c0c61b31a2814
Anish Aggarwal <misternyce@gmail.com>
2011-09-17 23:15:42 -0400

    push changes

--- themes/fadmashion_commerce/templates/footer.tpl.php
+++ themes/fadmashion_commerce/templates/footer.tpl.php
@@ -5,7 +5,8 @@
     <div class="subCol"><h1>Have a question?</h1><a href="mailto:support@fadmashion.com">Contact us anytime</a></div> 
     <div class="subCol"><h1>Share the deals</h1>
     
-      <a href="javascript:void(0);" onClick="javascript:fm_invite_facebookshare( '<?php print fm_invite_get_invite_url();?>', 'http://www.fadmashion.com/<?php print drupal_get_path("theme","fadmashion_commerce") . '/images/fb/fb-sequin-closeup.png'; ?>"><img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/icon_share_facebook.jpg"></a>
+      <?php $social_info = fm_deals_fb_social_info();?>
+      <a href="javascript:void(0);" onClick="javascript:fm_invite_facebookshare( '<?php print fm_invite_get_invite_url();?>', '<?php print $social_info['image_path']; ?>');"><img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/icon_share_facebook.jpg"></a>
       <img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/icon_share_twitter.jpg">
     </div>
 </div>
