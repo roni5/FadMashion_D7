7472414adfc7c573902b4977683009944ffc17a0
Anish Aggarwal <misternyce@gmail.com>
2011-10-25 14:44:31 -0400

    push changes

--- modules/custom/fm_image_slider/fm_image_slider_slides.js
+++ modules/custom/fm_image_slider/fm_image_slider_slides.js
@@ -4,21 +4,17 @@
  */
 
 jQuery(document).ready(function() {
-	jQuery('#slider').nivoSlider({
-        effect:'fade', // Specify sets like: 'fold,fade,sliceDown'
-        startSlide:0, // Set starting Slide (0 index)
-        directionNav:false, // Next & Prev navigation
-        directionNavHide:true, // Only show on hover
-        controlNav:showThumbs, // 1,2,3... navigation
-        controlNavThumbs: showThumbs, // Use thumbnails for Control Nav
-        controlNavThumbsFromRel:showThumbs, // Use image rel for thumbs
-        keyboardNav:true, // Use left & right arrows
-        manualAdvance:true,
-        pauseTime:6000, 
-    });
 	
-	if(!showThumbs) {
-		jQuery('.photos').addClass('no-thumbs');
-	}
-
+	$(function(){
+		$('#product').slides({
+			preload: true,
+			preloadImage: 'img/loading.gif',
+			effect: 'slide, fade',
+			crossfade: true,
+			slideSpeed: 350,
+			fadeSpeed: 500,
+			generateNextPrev: true,
+			generatePagination: false
+		});
+	});
 });
