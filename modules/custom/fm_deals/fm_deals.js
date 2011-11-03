e9b65909a9c642f0b407e4bb524e9412d9b7909e
Anish Aggarwal <misternyce@gmail.com>
2011-11-03 18:27:01 -0400

    push changes

--- modules/custom/fm_deals/fm_deals.js
+++ modules/custom/fm_deals/fm_deals.js
@@ -22,7 +22,9 @@ function fm_deals_facebookshare( url, thumb_path, description, name, message) {
  *  Javascript Tools to handle Forms in FAdmashion 
  */
 
+var WindowHasFocus = false;
 jQuery(document).ready(function() {
+	
 	dealTitleNotify();
 });
 
@@ -34,9 +36,11 @@ function dealTitleNotify() {
 	if(newTitle == null) {
 		newTitle = 'test';
 	}
-	jQuery.titleAlert(newTitle, {
-	    interval:900
-	});
+	if(!document.hasFocus()) {
+	  jQuery.titleAlert(newTitle, {
+	     interval:900
+	  });
+	}
 }
 
 
