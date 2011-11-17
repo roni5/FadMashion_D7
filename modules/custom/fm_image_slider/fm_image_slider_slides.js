93df0d67ecf7646d8756ca8b336ee0fdc8274334
Anish Aggarwal <misternyce@gmail.com>
2011-11-17 18:55:05 -0500

    push changes

--- modules/custom/fm_image_slider/fm_image_slider_slides.js
+++ modules/custom/fm_image_slider/fm_image_slider_slides.js
@@ -73,7 +73,7 @@ jQuery(document).ready(function() {
 		  
 		  if(boxHeight > max_picture_height) {
 			  boxHeight = max_picture_height;
-			  jQuery.colorbox.resize({height: max_picture_height + 130 });
+			  jQuery.colorbox.resize({height: max_picture_height + 160 });
 			  jQuery('#zoom_photo_tooltip').hide();
 		  }
 		  var middle = boxHeight/2;
