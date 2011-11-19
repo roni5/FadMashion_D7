340665cc62fd6350a78bbac6d410e9d675664888
Anish Aggarwal <misternyce@gmail.com>
2011-11-19 12:48:41 -0500

    push changes

--- themes/fadmashion_commerce/js/fm_onload.js
+++ themes/fadmashion_commerce/js/fm_onload.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function() {
 
 	jQuery('#block-views-nodequeue-2-block,  #block-views-nodequeue-3-block').addClass('basicPagePopUp');
 	
-	jQuery('#block-views-nodequeue-2-block  .views-slideshow-cycle-main-frame-row').each( function () {
+	jQuery('.basicPagePopUp .views-row').each( function () {
 	  var body_html = jQuery('.views-field-body', this).html();
 	  jQuery(' .views-field-title', this).append(body_html);
 	  jQuery('.views-field-body', this).html('');
