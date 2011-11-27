ac54f765b9a28a2c6fa69059b57e8b663cbee250
Anish Aggarwal <misternyce@gmail.com>
2011-11-27 12:56:12 -0500

    push changes

---
+++ themes/fadmashion_commerce/templates/page--deals.tpl.php
@@ -0,0 +1,39 @@
+<div align="center" id="wrapper">
+	<div class="container">
+<?php include('header.tpl.php');?>
+<script> fmCloseDropDown(); </script>
+          
+    <?php if(!empty($page['featured'])): ?>
+      <div id="sectionHeader">
+      <div class="pad">
+  			  <?php print render($page['featured']);?>
+  	  </div><div class="shadow">&nbsp;</div>
+  	  </div>
+      <?php endif;  ?> 
+      
+      
+	 <div id="content" class="sidebarRight">
+
+	   <div class="column1">   
+	   <?php if ($messages): ?>
+      <div id="messages"><div class="section clearfix">
+         <?php print $messages; ?>
+       </div></div> <!-- /.section, /#messages -->
+       <?php endif; ?>
+       
+	     <div class="mainContent">
+	        <?php print render($page['content']); ?>
+	     </div><!-- end mainContent -->
+	   </div> <!-- end column1 -->
+	   <div class="column2">
+	     <div class="sidebar">
+	       <?php print render($page['sidebar_second']); ?>
+	     </div>
+	   </div>
+	 </div>
+
+</div><!--wrapper-->
+</div><!--container-->
+
+<br clear="all"><br><br>
+<?php include('footer.tpl.php');?>
