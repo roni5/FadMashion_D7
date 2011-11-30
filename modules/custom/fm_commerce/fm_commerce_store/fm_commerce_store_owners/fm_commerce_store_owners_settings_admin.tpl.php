3dc51e796f3bcf4cca0a9e9e1c828b037a3a33b5
Anish Aggarwal <misternyce@gmail.com>
2011-11-30 13:35:59 -0500

    push changes

--- modules/custom/fm_commerce/fm_commerce_store/fm_commerce_store_owners/fm_commerce_store_owners_settings_admin.tpl.php
+++ modules/custom/fm_commerce/fm_commerce_store/fm_commerce_store_owners/fm_commerce_store_owners_settings_admin.tpl.php
@@ -5,7 +5,7 @@
      <!-- Paypal Business E-mail -->
      <table style="width: 100%">
        <tr style="height: 50px;">
-         <td>
+         <td style="width: 270px;" >
 	        <div id="paypal_header">
       	    <div>
 	            <img  class="paypal_logo" src=" <?php print pp(); ?>paypal_logo.gif">
@@ -14,19 +14,19 @@
 	        </div>
 	       </td>
 	   
-	      <td style="float: right; padding-top: 6px; ">
+	      <td style="float: left; padding-top: 6px; ">
     	    <?php print render(drupal_get_form('fm_commerce_store_owners_admin_form')); ?>
 	        <div class="error_container"></div>
 	      </td>
 	    </tr>
 	    
 	    <!-- Logo Settings-->
-	    <tr style="height: 50px;">
+	    <tr >
          <td>
 	         <div>Branding:</div>
 	       </td>
 	   
-	      <td style="float: right; padding-top: 6px; ">
+	      <td style="float: left; padding-top: 6px; ">
 	        <?php $logo = field_view_field('fm_commerce_store', $store, 'field_logo', 'node_full');
 	        print render($logo);?>
 	      </td>
