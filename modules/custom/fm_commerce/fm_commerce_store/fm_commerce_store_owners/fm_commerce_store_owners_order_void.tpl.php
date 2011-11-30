39d37a757856cc340496fd54eeab1d468575f4cd
Anish Aggarwal <misternyce@gmail.com>
2011-11-30 17:59:41 -0500

    push templates

---
+++ modules/custom/fm_commerce/fm_commerce_store/fm_commerce_store_owners/fm_commerce_store_owners_order_void.tpl.php
@@ -0,0 +1,4 @@
+<div style="width: 350px">
+<h2>This order has been voided.</h2>
+<p>Contact <a href="mailto:vendor-support@fadmashion.com">vendor-support@fadmashion.com</a> to get more info</p>
+</div>
