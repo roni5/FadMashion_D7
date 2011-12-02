0c61dac5c6c99077bdf193333f18c54252b738d8
Anish Aggarwal <misternyce@gmail.com>
2011-12-02 01:08:02 -0500

    push changes

---
+++ modules/custom/fm_commerce/fm_commerce_orders/fm_commerce_orders_demo.tpl.php
@@ -0,0 +1,154 @@
+<p style="font-style:italic; text-align: center; color: #B83838;"> Track your orders with ease. Check out some examples.</p>
+
+<!-- COPY RAW HTML BELOW HERE -->
+
+<div class="content">
+    <div id="orders-list"><table>
+ <thead><tr><th>Invoice</th><th>Details</th><th>Price</th><th>Status</th> </tr></thead>
+<tbody>
+ <tr class="odd"><td><div class="order-number">#91</div><div class="order-date">Sep 18</div></td><td><div class="product-title">Leather Ruffle Skirt</div> <div class="order-extra-info"><span class="order-size">Size 6</span></div></td><td><div class="sale-price">$486.50</div></td><td><div class="order-link"><span class="dotted-hover" ref="order-91" bt-xtitle="" title="">Shipped, Waiting for Payment</span></div></td> </tr>
+ <tr class="even"><td><div class="order-number">#83</div><div class="order-date">Sep 16</div></td><td><div class="product-title">Leather Shorts in Taupe</div> <div class="order-extra-info"><span class="order-color">Taupe </span></div></td><td><div class="sale-price">$339.50</div></td><td><div class="order-link"><span class="dotted-hover" ref="order-83" bt-xtitle="" title="">Transaction Complete!</span></div></td> </tr>
+ <tr class="odd"><td><div class="order-number">#82</div><div class="order-date">Sep 16</div></td><td><div class="product-title">Leather Shorts in Taupe</div> <div class="order-extra-info"><span class="order-color">Taupe </span></div></td><td><div class="sale-price">$339.50</div></td><td><div class="order-link"><span class="dotted-hover" ref="order-82" bt-xtitle="" title="">Transaction Complete!</span></div></td> </tr>
+ <tr class="even"><td><div class="order-number">#81</div><div class="order-date">Sep 16</div></td><td><div class="product-title">Leather Shorts in Taupe</div> <div class="order-extra-info"><span class="order-color">Taupe </span></div></td><td><div class="sale-price">$339.50</div></td><td><div class="order-link"><span class="dotted-hover" ref="order-81" bt-xtitle="" title="">Shipped, Waiting for Payment</span></div></td> </tr>
+ <tr class="odd"><td><div class="order-number">#80</div><div class="order-date">Sep 16</div></td><td><div class="product-title">Leather Shorts in Taupe</div> <div class="order-extra-info"><span class="order-color">Taupe </span></div></td><td><div class="sale-price">$339.50</div></td><td><div class="order-link"><span class="dotted-hover" ref="order-80" bt-xtitle="" title="">Order is Void</span></div></td> </tr>
+</tbody>
+</table>
+</div><div style="display: none;"><div id="content-order-91">
+
+<div class="orders_admin_wrapper">
+<h1>Shipped</h1>
+<p>We will be in touch shortly to process payment once we have confirmed the item has been shipped.  Thank you for your patience.</p>
+
+<div style="margin: 10px 0pt; overflow: hidden;">
+<div style="float: left; width: 146px; margin-right: 10px;" class="image">
+<img title="" alt="" src="http://fadmashion.com/sites/default/files/styles/fm_product_confirm/public/S11-1_0.jpg" typeof="foaf:Image" style="border: 1px solid rgb(226, 220, 214); width: 142px;"></div>
+
+<div style="float: left;">
+<h3>Leather Ruffle Skirt</h3>
+<div><div class="order-extra-info"><span class="order-size">Size 6</span></div></div>
+
+<div class="details" style="margin-top: 10px;">
+<div class="field field-name-commerce-customer-shipping field-type-commerce-customer-profile-reference field-label-hidden"><div class="field-items"><div class="field-item even"><div class="field field-name-field-shipping-address field-type-addressfield field-label-hidden"><div class="field-items"><div class="field-item even"><div class="xnal address-us">
+  <div class="street-address">
+    Anish Aggarwal<br>52 spring  </div>
+  <div>
+    <span class="locality">New York</span>
+    <span class="administrative_area">NY</span>
+    <span class="postal-code">11201</span>
+  </div>
+  <div>
+    <span class="country">United States</span>
+  </div>
+</div>
+</div></div></div></div></div></div></div>
+
+<div style="margin-top: 10px;">UPS: 112</div>
+
+</div>
+
+</div>
+</div>
+<div class="orders_popup_footer">If you have any questions, please contact us at <a href="mailto:support@fadmashion.com">support@fadmashion.com</a></div></div><div id="content-order-83">
+<div class="orders_admin_wrapper">
+<h1>Cha-Ching!</h1>
+<p>We have credited your Paypal business account with the correct amount.  </p>
+
+<div style="margin: 10px 0pt; overflow: hidden;">
+<div style="float: left; width: 146px; margin-right: 10px;" class="image">
+<img title="" alt="" src="http://fadmashion.com/sites/default/files/styles/fm_product_confirm/public/FadMashion_128.jpg" typeof="foaf:Image" style="border: 1px solid rgb(226, 220, 214); width: 142px;"></div>
+
+<div style="float: left;">
+<h3>Leather Shorts in Taupe</h3>
+<div><div class="order-extra-info"><span class="order-color">Taupe </span></div></div>
+
+<div class="details" style="margin-top: 10px;">
+<div class="field field-name-commerce-customer-shipping field-type-commerce-customer-profile-reference field-label-hidden"><div class="field-items"><div class="field-item even"><div class="field field-name-field-shipping-address field-type-addressfield field-label-hidden"><div class="field-items"><div class="field-item even"><div class="xnal address-us">
+  <div class="street-address">
+    Anish Aggarwal<br>52 Spring  </div>
+  <div>
+    <span class="locality">New york</span>
+    <span class="administrative_area">NY</span>
+    <span class="postal-code">11201</span>
+  </div>
+  <div>
+    <span class="country">United States</span>
+  </div>
+</div>
+</div></div></div></div></div></div></div>
+
+</div>
+
+
+
+</div></div><div class="orders_popup_footer">If you have any questions, please contact us at <a href="mailto:support@fadmashion.com">support@fadmashion.com</a></div></div><div id="content-order-82">
+<div class="orders_admin_wrapper">
+<h1>Cha-Ching!</h1>
+<p>We have credited your Paypal business account with the correct amount.  </p>
+
+<div style="margin: 10px 0pt; overflow: hidden;">
+<div style="float: left; width: 146px; margin-right: 10px;" class="image">
+<img title="" alt="" src="http://fadmashion.com/sites/default/files/styles/fm_product_confirm/public/FadMashion_128.jpg" typeof="foaf:Image" style="border: 1px solid rgb(226, 220, 214); width: 142px;"></div>
+
+<div style="float: left;">
+<h3>Leather Shorts in Taupe</h3>
+<div><div class="order-extra-info"><span class="order-color">Taupe </span></div></div>
+
+<div class="details" style="margin-top: 10px;">
+<div class="field field-name-commerce-customer-shipping field-type-commerce-customer-profile-reference field-label-hidden"><div class="field-items"><div class="field-item even"><div class="field field-name-field-shipping-address field-type-addressfield field-label-hidden"><div class="field-items"><div class="field-item even"><div class="xnal address-us">
+  <div class="street-address">
+    Anish Aggarwal<br>52 Spring  </div>
+  <div>
+    <span class="locality">New york</span>
+    <span class="administrative_area">NY</span>
+    <span class="postal-code">11201</span>
+  </div>
+  <div>
+    <span class="country">United States</span>
+  </div>
+</div>
+</div></div></div></div></div></div></div>
+
+</div>
+
+
+
+</div></div><div class="orders_popup_footer">If you have any questions, please contact us at <a href="mailto:support@fadmashion.com">support@fadmashion.com</a></div></div><div id="content-order-81">
+
+<div class="orders_admin_wrapper">
+<h1>Shipped</h1>
+<p>We will be in touch shortly to process payment once we have confirmed the item has been shipped.  Thank you for your patience.</p>
+
+<div style="margin: 10px 0pt; overflow: hidden;">
+<div style="float: left; width: 146px; margin-right: 10px;" class="image">
+<img title="" alt="" src="http://fadmashion.com/sites/default/files/styles/fm_product_confirm/public/FadMashion_128.jpg" typeof="foaf:Image" style="border: 1px solid rgb(226, 220, 214); width: 142px;"></div>
+
+<div style="float: left;">
+<h3>Leather Shorts in Taupe</h3>
+<div><div class="order-extra-info"><span class="order-color">Taupe </span></div></div>
+
+<div class="details" style="margin-top: 10px;">
+<div class="field field-name-commerce-customer-shipping field-type-commerce-customer-profile-reference field-label-hidden"><div class="field-items"><div class="field-item even"><div class="field field-name-field-shipping-address field-type-addressfield field-label-hidden"><div class="field-items"><div class="field-item even"><div class="xnal address-us">
+  <div class="street-address">
+    Anish Aggarwal<br>52 Spring  </div>
+  <div>
+    <span class="locality">New york</span>
+    <span class="administrative_area">NY</span>
+    <span class="postal-code">11201</span>
+  </div>
+  <div>
+    <span class="country">United States</span>
+  </div>
+</div>
+</div></div></div></div></div></div></div>
+
+<div style="margin-top: 10px;">: </div>
+
+</div>
+
+</div>
+</div>
+<div class="orders_popup_footer">If you have any questions, please contact us at <a href="mailto:support@fadmashion.com">support@fadmashion.com</a></div></div><div id="content-order-80"><div class="orders_popup_footer" style="margin-top: 0px;">If you have any questions, please contact us at <a href="mailto:support@fadmashion.com">support@fadmashion.com</a></div></div></div>  </div>
+
+<!-- END OF PASTE -->
+
+
