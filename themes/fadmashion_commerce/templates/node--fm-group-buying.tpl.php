1d5bbdf86881365efeb4e3a20763579a9ac1c230
Anish Aggarwal <misternyce@gmail.com>
2011-12-08 00:44:40 -0500

    push changes

--- themes/fadmashion_commerce/templates/node--fm-group-buying.tpl.php
+++ themes/fadmashion_commerce/templates/node--fm-group-buying.tpl.php
@@ -83,7 +83,7 @@
   <?php print render($content['product:field_product_images']); ?>
 </div>
 <div class="info">
-<h1 class="pageTitle">1-HOUR  <span>DEALS</span></h1>
+<h1 class="pageTitle">1-HOUR  <span>EXCLUSIVE</span></h1>
 <?php print $groupDetails;?>
 <br clear="all">
  <?php print $product_details; ?> 
