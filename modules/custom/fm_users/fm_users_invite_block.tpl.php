34ee6a5d7dee3c22ddeb997bde5533cc71ccd42c
Anish Aggarwal <misternyce@gmail.com>
2011-12-09 01:14:22 -0500

    push changes

--- modules/custom/fm_users/fm_users_invite_block.tpl.php
+++ modules/custom/fm_users/fm_users_invite_block.tpl.php
@@ -4,7 +4,7 @@
 
 
 <h1>Invite Friends</h1>
-<p>As a member, we have provided you with aprivate invite code that gives <b>insider access</b> on Fadmashion.com to your friends, family and biggest fans .  </p>
+<p style="width: 500px; margin: 0 auto;">As a member, we have provided you with a <b>private invite code</b></b> that gives <b>insider access</b> on Fadmashion.com to your friends, family and biggest fans.  </p>
 
 <table cellspacing="0" cellpadding="0" class="rounded-top" id="inviteCode">
 <tbody><tr>
