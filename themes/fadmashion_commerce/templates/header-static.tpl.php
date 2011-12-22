ec26fd2e6a25a5ae20a278abbebac6294720d327
Anish Aggarwal <misternyce@gmail.com>
2011-12-22 00:53:35 -0500

    push changes

--- themes/fadmashion_commerce/templates/header-static.tpl.php
+++ themes/fadmashion_commerce/templates/header-static.tpl.php
@@ -16,7 +16,7 @@
           ?>
    
        <a class="logo" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" >
-         <img src="<?php print pp();?>logo_fadmashion.png" alt="<?php print t('fadmashion'); ?>" />
+         <img  src="<?php print pp();?>logo_fadmashion.png" alt="<?php print t('fadmashion'); ?>" />
       </a>
     </div>
  </div>
