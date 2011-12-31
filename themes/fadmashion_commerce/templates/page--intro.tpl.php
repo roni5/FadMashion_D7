244edbfd6148207c63ce9c26fb1a127bf07c6c9f
Anish Aggarwal <misternyce@gmail.com>
2011-12-31 11:48:01 -0500

    push changes

--- themes/fadmashion_commerce/templates/page--intro.tpl.php
+++ themes/fadmashion_commerce/templates/page--intro.tpl.php
@@ -63,7 +63,7 @@ jQuery(function($){
 	        	<a  href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" >
         			    <img src="/<?php print drupal_get_path("theme","fadmashion_commerce");?>/images/logo_fadmashion.png" alt="<?php print t('fadmashion'); ?>" />
       			    </a>
-      			 <div class="slogan"><?php print $site_slogan; ?></div>   
+      			 <div class="slogan"><?php //print $site_slogan; ?></div>   
         </div>
         <?php if ($messages): ?>
           <div id="messages"><div class="section clearfix">
