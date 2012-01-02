50e57225d0972641edc2475fc25c520c56f3549b
Anish Aggarwal <misternyce@gmail.com>
2012-01-02 12:34:58 -0500

    push changes

---
+++ modules/custom/fm_users/fm_users_register/fm_users_register_splash.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+jQuery(document).ready(function() {
+	
+	  if(jQuery('#fm-users-register-intro-form').length) {
+		   
+		jQuery('#fm-users-register-intro-form').validate({
+			errorClass: 'invalid',
+	    	errorContainer: jQuery(".errorText"),
+	  	    onkeyup: false,
+	  	    wrapper: 'div id="message_box"',
+	     });
+	  
+	    jQuery("#fm-users-register-intro-form #edit-mail").rules("add", {
+	 	   notEqual: "E-mail",
+	 	   email: true,
+	     });
+		  
+	 }
+	  
+
+});
