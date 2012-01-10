e78140024504ddf14f7b0190795d6913be264177
Anish Aggarwal <misternyce@gmail.com>
2012-01-09 22:26:18 -0500

    asdfa

--- modules/custom/fm_image_slider/fm_image_slider_slides.js
+++ modules/custom/fm_image_slider/fm_image_slider_slides.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function() {
 		jQuery('#product').slides({
 			preload: true,
 			preloadImage: loadingImagePath,
-			effect: 'slide, fade',
+			effect: 'fade',
 			crossfade: true,
 			slideSpeed: slideSpeed,
 			fadeSpeed: fadeSpeed,
