8a2a761fc894969e5214ddb0ac2faa12e5b9567e
Anish Aggarwal <misternyce@gmail.com>
2012-03-25 14:01:55 -0400

    push new welcome message

--- modules/custom/fm_users/fm_users_invite_block.tpl.php
+++ modules/custom/fm_users/fm_users_invite_block.tpl.php
@@ -7,12 +7,13 @@
 <p style="width: 460px; margin: 0 auto; font-weight: 500">We have provided you with a private invite code that gives insider access for your <b>friends, family and biggest fans</b>.  </p>
 </div>
 
+
 <div class="new_signup" style="display:none;">
 <h1>Welcome to Fadmashion!</h1>
-<p style="width: 480px; margin: -15px auto 0; font-weight: 500"> Below is your personal invite code that you can <b>share on Facebook, Twitter or send directly to your friend's inbox</b>.  We keep track of each friend who signs up and will be offering loyalty benefits for fashion show VIP tix, parties, exclusive discounts and much more.</p>
+<p style="width: 500px; margin: -15px auto 0; font-weight: 500">  We are growing community of trendsetters whose common passion celebrates individuality and origniality. Follow us on Facebook to get coverage on the hottest designer collections, fashion shows, and special sale annoucements.</p>
+<fb:like style="margin-top: 10px;" href="http://www.facebook.com/Fadmashion" send="true" width="300" show_faces="false"></fb:like>
 
 </div>
-
 
 <table cellspacing="0" cellpadding="0" class="rounded-top" id="inviteCode">
 <tbody><tr>
@@ -33,7 +34,12 @@
 
 </div>
 
-<fb:like style="margin-top: 10px;" href="http://www.facebook.com/pages/FadMashion/8251371790" send="true" width="300" show_faces="false"></fb:like>
+<div class="old_signup">
+<fb:like style="margin-top: 10px;" href="http://www.facebook.com/Fadmashion" send="true" width="300" show_faces="false"></fb:like>
+</div>
+
+
+
 
 </div> <!-- box -->
 </div> <!-- state2 -->
