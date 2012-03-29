4b2f00d1930f97a293cab2419a42bda7da38e3bb
Anish Aggarwal <misternyce@gmail.com>
2012-03-29 13:26:18 -0400

    push changes

--- modules/custom/fm_shop/fm_shop_page_panel_thumb.tpl.php
+++ modules/custom/fm_shop/fm_shop_page_panel_thumb.tpl.php
@@ -1,5 +1,10 @@
 <div id="" class="capslide_img_cont ic_container" style= "height: <?php print $height ?>px; width: <?php print $width ?>px">
-  <a href="##"><?php print fm_commerce_product_image_thumb($product, 'fm_main_product_image', array('style' => 'height: '. $height . 'px; width: ' . $width . 'px;')); ?>
+  <?php $img = fm_commerce_product_image_thumb($product, 'fm_main_product_image', array('style' => 'height: '. $height . 'px; width: ' . $width . 'px;')); 
+
+      $node = fm_commerce_get_display_node($product);
+        print l($img, 'node/' . $node->nid, array('html' => true));
+  
+  ?>
      <div class="overlay" style="display:none;"></div>
        <div class="ic_caption">
            <p class="ic_category"><?php print $product->title; ?></p>
