8771610da3e26022abd32b84cc498acbd1fb5e90
Anish Aggarwal <misternyce@gmail.com>
2012-03-30 15:39:47 -0400

    push cahnges

--- themes/v15_fadmashion_commerce/js/fm_onload.js
+++ themes/v15_fadmashion_commerce/js/fm_onload.js
@@ -79,6 +79,29 @@ jQuery(document).ready(function() {
              });
          	
          	var galleries = jQuery('.ad-gallery').adGallery();
+         	jQuery('#switch-effect').change(
+         		      function() {
+         		        galleries[0].settings.effect = $(this).val();
+         		        return false;
+         		      }
+         		    );
+         	jQuery('#toggle-slideshow').click(
+         		      function() {
+         		        galleries[0].slideshow.toggle();
+         		        return false;
+         		      }
+         		    );
+         	jQuery('#toggle-description').click(
+         		      function() {
+         		        if(!galleries[0].settings.description_wrapper) {
+         		          galleries[0].settings.description_wrapper = $('#descriptions');
+         		        } else {
+         		          galleries[0].settings.description_wrapper = false;
+         		        }
+         		        return false;
+         		      }
+         		    );
+
 
          };
          
