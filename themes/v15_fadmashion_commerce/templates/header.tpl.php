bd0699af04e301569b3a3f2c8c8389fa4e737b61
Anish Aggarwal <misternyce@gmail.com>
2012-04-02 15:11:10 -0400

    push changes

--- themes/v15_fadmashion_commerce/templates/header.tpl.php
+++ themes/v15_fadmashion_commerce/templates/header.tpl.php
@@ -61,5 +61,5 @@
         	<a class="press2" href="#">My Fashion Life</a>
         	<a class="press3" href="#">FashionSpot</a>
             </div>
-        <div class="slogan"><span>Browse</span> designer collections.&nbsp;&nbsp;&nbsp;<span>Shop</span> private sales.&nbsp;&nbsp;&nbsp;<span>Look</span> better than your friends.</div>
+        <div class="slogan"><span>Browse</span> designer collections.&nbsp;&nbsp;&nbsp;<span>Shop</span> private sales.&nbsp;&nbsp;&nbsp;<span>Love</span> your new look.</div>
         </div>
