2c8d47f8716c1a96368f7d8c84f18f0fd7919105
Anish Aggarwal <misternyce@gmail.com>
2012-04-13 13:03:34 -0400

    push cahnges

---
+++ themes/v15_fadmashion_commerce/templates/header-blog.tpl.php
@@ -0,0 +1,26 @@
+
+	 
+	 
+<div id="header">
+  <div class="right">
+    <div class="links">
+        
+    </div><!-- links-->
+    <div class="shipping"><p><span>Free shipping</span> on orders of $150 or more, plus hassle-free returns</p></div></div>
+        
+    <a class="logo" href="<?php print $front_page; ?>" title="<?php print t('Fadmashion'); ?>" rel="home" >
+        			    <img src="<?php print pp();?>logo_fm.png" alt="<?php print t('Fadmashion'); ?>" />
+    </a>
+    <div class="nav">
+     <?php print l('Shop', 'shop', array('absolute' => true ));?>
+      <a href="http://blog.fadmashion.com" class="active">Discover</a>              
+    </div>
+</div>
+
+<div id="subHeader">
+        <div class="press">
+        	<a class="press1" href="#">NY Mag</a>
+        	<a class="press3" href="#">FashionSpot</a>
+            </div>
+        <div class="slogan"><span>Insider access</span> to fashion's hottest designer collections.</div>
+        </div>
---
+++ themes/v15_fadmashion_commerce/templates/page--blog-header.tpl.php
@@ -0,0 +1,7 @@
+<div id="wrapper">
+	<div id="container">
+<?php include('header-blog.tpl.php');?>
+            
+
+</div><!--wrapper-->
+</div><!--container-->
