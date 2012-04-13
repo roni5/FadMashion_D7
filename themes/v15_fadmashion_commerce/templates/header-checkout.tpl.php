31bbc1ca2936da758c817ed6e298ad786662ee88
Anish Aggarwal <misternyce@gmail.com>
2012-04-12 22:01:29 -0400

    push changes

--- themes/v15_fadmashion_commerce/templates/header-checkout.tpl.php
+++ themes/v15_fadmashion_commerce/templates/header-checkout.tpl.php
@@ -1,9 +1,10 @@
-<div class="express-checkout" id="header">
-  <div class="row2">
-     <?php print l('Continue Shopping', '', array('attributes' => array('class' => array('button') ) ));?>
-     <a class="logo" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" >
-         <img  src="<?php print pp();?>logo_fadmashion.png?v2" alt="<?php print t('fadmashion'); ?>" />
-      </a>
-  <div class="title">Express Checkout</div>
-  </div>
+
+
+<div id="header">
+    <a class="logo" href="<?php print $front_page; ?>" title="<?php print t('Fadmashion'); ?>" rel="home" >
+        			    <img src="<?php print pp();?>logo_fm.png" alt="<?php print t('Fadmashion'); ?>" />
+    </a>
+    <div class="nav">
+      <div class="title">Express Checkout</div>        
+    </div>
 </div>
--- themes/v15_fadmashion_commerce/templates/page--express-checkout.tpl.php
+++ themes/v15_fadmashion_commerce/templates/page--express-checkout.tpl.php
@@ -1,18 +1,26 @@
-<div align="center" id="wrapper">
-	<div class="container">
+<div id="wrapper">
+	<div id="container">
 <?php include('header-checkout.tpl.php');?>
+            
+    <?php if(!empty($page['featured'])): ?>
+      <div id="sectionHeader">
+      <div class="pad">
+  			  <?php print render($page['featured']);?>
+  	  </div><div class="shadow">&nbsp;</div>
+  	  </div>
+      <?php endif;  ?> 
+      
+      
 	 <div id="content" class="sidebarRight">
-	   <div class="column1">
-	     
-<?php if ($messages): ?>
+     <?php if ($messages): ?>
       <div id="messages"><div class="section clearfix">
          <?php print $messages; ?>
        </div></div> <!-- /.section, /#messages -->
        <?php endif; ?>
-  
-	        <?php print render($page['content']); ?>
-	   </div> <!-- end column1 -->
-	   <div class="column2">
+       <?php print render($page['content']); ?>
+	   
+	 
+	    <div class="column2">
 	     <div class="sidebar">
 	       <?php print render($page['sidebar_second']); ?>
 	     </div>
