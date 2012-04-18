2ba5f1451d75325cc4f681d084c93c75a731384c
Anish Aggarwal <misternyce@gmail.com>
2012-04-17 23:31:29 -0400

    push changes

--- modules/custom/fm_social/fm_social_favorite/fm_social_favorite.js
+++ modules/custom/fm_social/fm_social_favorite/fm_social_favorite.js
@@ -9,7 +9,18 @@
 		  
 		  jQuery('.my_favorites li a').address();
 		  
-		  jQuery(".social_favorites a").once('init-social-favorites-processed').click(function (e){
+		  jQuery('.my_favorites_containter .close').once('init-closed-processed').click(function (e){
+			  jQuery('.my_favorites_containter').slideToggle('5000', function() {
+				  jQuery('.my_favorites_link').fadeIn();
+			  });
+		  });
+		  jQuery('.my_favorites_link a').once('init-open-processed').click(function (e){
+			  e.preventDefault();
+			  jQuery('.my_favorites_link').fadeOut('slow');
+				  jQuery('.my_favorites_containter').slideToggle('5000');
+		  });
+		  
+		  jQuery(".favorite_button").once('init-social-favorites-processed').click(function (e){
 				e.preventDefault();
 				var href_val = jQuery(this).attr('href');
 				var product_id = jQuery(this).attr('id');
@@ -32,6 +43,7 @@
 					}  
 				});
 			});
+		
 	  }
 	};
 })(jQuery);
