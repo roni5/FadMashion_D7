72a5df40a92c9cc779fe9f4b5df7a8d722de069d
Anish Aggarwal <misternyce@gmail.com>
2012-04-27 13:48:07 -0400

    pushc ahnges

--- modules/custom/fm_users/fm_users_register/fm_users_register_footer_tabs_promo.tpl.php
+++ modules/custom/fm_users/fm_users_register/fm_users_register_footer_tabs_promo.tpl.php
@@ -1,7 +1,7 @@
 <div id="register_footer_promo">
 <div class="text">
 <h1><span>Insider Access</span> to Fashion's Hottest Designer Collections. </h1>
-<h2>Get $10 instant credits + an addtional $10 for each of your invited friends.</h2>
+<h2>Get $10 instant credits + an additional $10 after the first purchase of friends you invite.</h2>
 </div>
 <div class="register">
   <a class="button" href="javascript:void(0);" onClick="fmUserStateRestart(); jQuery('.state1').show();fmForceRegister();fmClearRegisterTimeout();">Sign Up</a>
