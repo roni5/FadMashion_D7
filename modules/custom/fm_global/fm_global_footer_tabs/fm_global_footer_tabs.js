6b6bf1874454e77882d068221679e2a3cd1f54fa
Anish Aggarwal <misternyce@gmail.com>
2012-04-27 16:19:26 -0400

    push changes

--- modules/custom/fm_global/fm_global_footer_tabs/fm_global_footer_tabs.js
+++ modules/custom/fm_global/fm_global_footer_tabs/fm_global_footer_tabs.js
@@ -9,23 +9,27 @@ jQuery(document).ready(function() {
 	  var tabs_content = jQuery('.footer_tabs_content');
 	  var tabs_links = jQuery('.footer_tabs_links');
 	  tabs_content.resize( function() {
-		 var height = tabs_content.outerHeight() - 13;
-		 tabs_links.css('bottom', height + 'px');
+		  if(tabs_content.is(':visible')) {
+             var height = tabs_content.outerHeight() - 13;
+		     tabs_links.css('bottom', height + 'px');
+		  }
 	  });
 	 
 	jQuery('.footer_tabs_closes').once('init-closed-processed').click(function (e){
 		  e.preventDefault();
 		  
-
-		  if(tabs_links.hasClass('collapsed')) {
-			  tabs_links.removeClass('collapsed');
-		  } 
 			  
 		  tabs_content.slideToggle('5000', function() {
 			  if(!tabs_links.hasClass('collapsed') && !tabs_content.is(':visible')) {
 			    tabs_links.addClass('collapsed'); 
 		     }
 		  });
+		  
+
+
+		  if(tabs_links.hasClass('collapsed')) {
+			  tabs_links.removeClass('collapsed');
+		  } 
 
 	  });
 	
