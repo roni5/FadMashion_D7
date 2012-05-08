3331cef754ff6454cca4b21ee6912c9fbb3127ba
Anish Aggarwal <misternyce@gmail.com>
2012-05-08 11:49:27 -0400

    push changes

--- modules/custom/fm_users/fm_users_credits/fm_users_credits_footer_tab.tpl.php
+++ modules/custom/fm_users/fm_users_credits/fm_users_credits_footer_tab.tpl.php
@@ -1,10 +1,26 @@
 <div id="footer_credits_promo">
+
+<div class="quick_links">
+
+<div class="col">
+<?php global $user; print $picture = theme('user_picture', array('account' => $user)) ;?></div>
+<div class="col">
+<div>Welcome, <?php print fm_users_firstname();?>!</div>
+<div><?php print l('(Logout)', 'user/logout');?></div>
+</div>
+
+<div class="col">
+<div><?php print l('Order Status', '');?></div>
+<div><?php print l('Settings', '');?></div>
+</div>
+</div>
+
+
 <div class="credits">
 <h1>You have<span><?php print $credits?></span></h1>
 <div style="font-size: 11px; color: rgb(255, 255, 255); margin-top: -6px;"> worth of credits</div>
-
-
 </div>
+
 <div class="invite_friends">
   <div class="text">1 Friend's Purchase = $10 worth of credits
   <?php $url = '/?inline=true#block-fm-users-fm-users-invite&blankBox=1';
