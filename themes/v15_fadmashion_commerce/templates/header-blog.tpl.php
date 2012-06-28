c218097cf455aef8e6d54734bccc838f05a5c7b2
Anish Aggarwal <misternyce@gmail.com>
2012-06-28 16:06:57 -0400

    push changes

--- themes/v15_fadmashion_commerce/templates/header-blog.tpl.php
+++ themes/v15_fadmashion_commerce/templates/header-blog.tpl.php
@@ -21,7 +21,7 @@
     </a>
     <div class="nav">
      <?php  // print l('Shop', 'shop', array('absolute' => true, 'attributes' => array('target' => '_parent') ));?>
-      <a target="_parent" href="http://www.fadmashion.com" class="active">Shop</a>
+      <a target="_parent" href="http://www.fadmashion.com" >Shop</a>
       <a target="_parent" href="http://blog.fadmashion.com" class="active">Discover</a>              
     </div>
 </div>
