0b26ce439f757a32f14ef52e8f3d95e86bc20b7b
Anish Aggarwal <misternyce@gmail.com>
2012-06-28 16:08:58 -0400

    push changes

--- themes/v15_fadmashion_commerce/templates/header-blog.tpl.php
+++ themes/v15_fadmashion_commerce/templates/header-blog.tpl.php
@@ -8,7 +8,7 @@
         <?php global $user;
             if ($user->uid) { ?>
                 
-                <?php print l('Logout', 'user/logout', array('attributes' => array('parent' => '_target')));?>
+                <?php print l('Logout', 'user/logout', array('attributes' => array('target' => '_parent')));?>
              <?php } else{ ?>
                <a  href="javascript:void(0);" onClick="fmUserStateRestart(); jQuery('.state1').show();fmForceRegister();fmClearRegisterTimeout();">Sign Up</a>
                <a href="javascript:void(0);" onClick="fmUserStateRestart(); jQuery('.state3').show();fmForceRegister();fmClearRegisterTimeout();">Login</a>
