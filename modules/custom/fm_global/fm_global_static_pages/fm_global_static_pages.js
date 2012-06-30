8ec6cfc35e70808bfb809e9ca61fc4d9fee197fa
Anish Aggarwal <misternyce@gmail.com>
2012-06-29 21:39:55 -0400

    asdf

---
+++ modules/custom/fm_global/fm_global_static_pages/fm_global_static_pages.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+
+jQuery(document).ready(function() {
+	
+	//ADD deep linking, 
+	 jQuery.address.crawlable(true).init(function(event) {
+
+         // Initializes plugin support for links
+		 jQuery('.page-static .col1 a').address();
+
+
+     })
+     .change(function(event) {
+
+         // Identifies the page selection
+    	 var static_id = 'about-us';
+    	 if(event.pathNames[0]) {
+		   static_id = event.pathNames[0];
+	     }
+    	 
+    	 jQuery('.page-static .col1 a').each(function() {
+			 if (jQuery(this).attr('id') == static_id) {
+			    jQuery(this).addClass('active').focus();
+           } else {
+         	jQuery(this).removeClass('active');
+           }
+         });
+    	 
+    	 data = jQuery('#cache .' + static_id).html();
+     	
+     	
+     	dataPage = jQuery('.page-static .col2 .pad');
+     	dataPage.hide();
+    	dataPage.html(data);
+    	dataPage.fadeTo('fast', 1);
+     	
+        
+     });
+ });
