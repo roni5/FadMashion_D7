0193c72744602d6d342030380e8589d168ad3421
Anish Aggarwal <misternyce@gmail.com>
2012-07-13 23:20:31 -0400

    asdfaf

--- modules/custom/fm_shop/fm_shop_page.tpl.php
+++ modules/custom/fm_shop/fm_shop_page.tpl.php
@@ -1,9 +1,9 @@
 
 <div class="col1">
   <div class="pad">
-  <ul>
+  <ul style="overflow: hidden;">
     <li><?php  print l('Home', 'shop/all', array('attributes' => array('id' => 'all', 'class' => array('active', 'viewAll')))) ?></li>
-    <li><?php $id = 'all-time'; print l('Most Loved', 'shop', array('attributes' =>  array('id' => $id, 'class' => array('active')), 'query' => array('favorites' => $id)) ) ?></li>
+    <li><img style="float: left" src="<?php print pp()?>icon_star.png"><?php $id = 'all-time'; print l('Most Loved', 'shop', array('attributes' =>  array('id' => $id, 'class' => array('active')), 'query' => array('favorites' => $id)) ) ?></li>
   </ul>
   <?php foreach($filters as  $filter_group) {
   	if(!empty($filter_group['title'] )) {
