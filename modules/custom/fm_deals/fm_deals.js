ddc1e0fabb5c01623b8576dabad4d0d6c88039c2
Anish Aggarwal <misternyce@gmail.com>
2012-07-16 19:54:05 -0400

    push changes

--- modules/custom/fm_deals/fm_deals.js
+++ modules/custom/fm_deals/fm_deals.js
@@ -5,7 +5,7 @@
 /* Share functions */
 function fm_deals_facebookshare( url, thumb_path, description, name, message) {
 	 FB.ui({
-       method: 'feed',
+       method: 'send',
 	   app_id: '213872101957329',
 	   name: name,
 	   link: url,
