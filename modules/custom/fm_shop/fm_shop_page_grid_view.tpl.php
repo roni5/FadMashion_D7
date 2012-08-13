691064383036e8f05f220c3cdbfa7c2222a4886a
Anish Aggarwal <misternyce@gmail.com>
2012-08-13 00:13:04 -0400

    push changes

--- modules/custom/fm_shop/fm_shop_page_grid_view.tpl.php
+++ modules/custom/fm_shop/fm_shop_page_grid_view.tpl.php
@@ -6,8 +6,25 @@
   <img src="<?php print pp();?>divider_large.jpg">
 </div>
 <div id="grid">
-  <?php foreach($products as $product) {
-    print theme('fm_shop_page_grid_view_item', array('product' => $product, 'argument' => $argument));
-  }?>
+
+  <?php 
+  $row_num = 4;
+  $rows = array();
+  $i = 0; $j = 0;
+  foreach($products as $product) {
+  	if(!($i % 4)) {
+  		$j++;
+  	}
+    $rows[$j][] = $product;
+  }
+  foreach($rows as $row) {
+  	print '<div class="row">';
+  	foreach($row as $product) {
+  		print theme('fm_shop_page_grid_view_item', array('product' => $product, 'argument' => $argument));
+  	}
+  	print '</div>';
+  }
+  
+  ?>
 </div>
 </div>
