bff32194b2575d9f2047812b8018c8dd656cc173
Anish Aggarwal <misternyce@gmail.com>
2012-08-13 12:16:09 -0400

    asdfa

--- modules/custom/fm_shop/fm_shop.js
+++ modules/custom/fm_shop/fm_shop.js
@@ -355,33 +355,12 @@ jQuery(document).ready(function() {
 			   	onkeyup: false,
 			   	wrapper: 'div id="message_box"',
 		  });
-		 /* 	 
-		 var sizes = jQuery("#commerce-cart-add-to-cart-form #edit-sizes")
-		  if(sizes.length) {
-			 sizes.rules("add", {
-		       required: true,
-		       messages: {
-		    	   required: "Select a size",
-		           }
-		       });
-		  }
-			 
-	     var colors = jQuery("#commerce-cart-add-to-cart-form #edit-colors");
-		 if(colors.length) {
-			 colors.rules("add", {
-		   	   required: true,
-		   	   messages: {
-		   		   required: "Select a Color",
-		           }
-		       });
-		    }*/
-		 
 
 		//Enable Javascript Color functionality for Fadmashion sizes
-		jQuery('.form-item-sizes select').once('init-selectbox-processed').selectBox();
+		jQuery('.form-item-sizes select').selectBox();
 		
 		//Enable Javascript Color functionality for Fadmashion Commerce
-		jQuery('.form-item-colors select').once('init-selectbox-processed').selectBox();
+		jQuery('.form-item-colors select').selectBox();
 				
 		//Change color options to div structures
 		jQuery('.form-item-colors .selectBox-options li a').each(function() {
