9ff0ea9746567a46827f75587b48bef90f78b958
Anish Aggarwal <misternyce@gmail.com>
2012-08-13 16:44:25 -0400

    asdf

--- modules/custom/fm_home/fm_home_browse_panel.tpl.php
+++ modules/custom/fm_home/fm_home_browse_panel.tpl.php
@@ -3,6 +3,6 @@
             <div class="pad">
             <a class="browseDesigners" href="#">Browse Designers</a>
                 <img src="<?php print pp();?>p_divider_dark.png">
-            <a class="browseMostLoved" href="#">Browse Most Loved</a>
+            <?php print l('Browse Most Loved', 'shop', array('attributes' => array('class' => 'browseMostLoved') ));?>
             </div></div>
             </div>
--- modules/custom/fm_home/fm_home_invite_panel.tpl.php
+++ modules/custom/fm_home/fm_home_invite_panel.tpl.php
@@ -4,6 +4,6 @@
                 <img src="<?php print pp();?>p_instantcredits.png">
                 <p>plus $10 after your friends make their first purchase!</p>
                 <img src="<?php print pp();?>p_divider_light.png">
-                <a class="button" href="#">Join Free</a>
+                <a class="button" href="javascript:void(0);" onClick="fmUserStateRestart(); jQuery('.state1').show();fmForceRegister();fmClearRegisterTimeout();">Join Free</a>
             </div>
             </div>
