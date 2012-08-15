d8db0ff7400327dacf0dfcd15714b39026ed8b69
Anish Aggarwal <misternyce@gmail.com>
2012-08-14 23:31:47 -0400

    push changes

--- modules/custom/fm_shop/fm_shop_page_grid_view_item.tpl.php
+++ modules/custom/fm_shop/fm_shop_page_grid_view_item.tpl.php
@@ -9,9 +9,13 @@
     $query_array = array_merge($argument ,array('nid' => $node->nid));
     $link = l($img, 'node/' . $node->nid, array('html' => true, 'attributes' => array('id' => $node->nid, 'class' => 'thumb_link'), 'query' => $query_array ));
     print $link;
+    
+    $result = db_query('SELECT count(*) as count FROM fm_social_favorite WHERE product_id = :product_id', array(':product_id' => $product->product_id));
+  $obj = $result->fetchObject();
+  
   ?>       	  
     <h1><?php print l($node->title, 'node/' . $node->nid, array('query' => $query_array ));?></h1>
    <h2><span><?php node_build_content($node); print render($node->content['field_sale_price']); ?></span></h2>
-
+   <h3><?php print $obj->count ?></h3>
 <?php } ?>
 </div>
