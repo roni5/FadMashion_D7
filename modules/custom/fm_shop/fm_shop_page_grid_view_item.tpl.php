d87fc6ac8f12dd3e74117ef69302a221ffd9b97c
Anish Aggarwal <misternyce@gmail.com>
2012-08-15 15:23:06 -0400

    push changes

--- modules/custom/fm_shop/fm_shop_page_grid_view_item.tpl.php
+++ modules/custom/fm_shop/fm_shop_page_grid_view_item.tpl.php
@@ -1,9 +1,11 @@
-<div class="block">
- 
 <?php 
   $img =  fm_commerce_product_image_thumb($product, 'fm_product_grid_view', array('style' => 'height: '. $height . 'px; width: ' . $width . 'px;'));
   $node = fm_commerce_get_display_node($product);  
   if(isset($node)) {
+?>
+<div class="block">
+<?php 
+  	
     $shop = fm_commerce_get_store($product);
     
     $query_array = array_merge($argument ,array('nid' => $node->nid));
@@ -19,5 +21,6 @@
     <h1><?php print l($node->title, 'node/' . $node->nid, array('query' => $query_array ));?></h1>
    <h2><span><?php node_build_content($node); print render($node->content['field_sale_price']); ?></span></h2>
    <?php print ($obj->count ? '<h3>' . $obj->count . '</h3>': ''); ?>
+   </div>
+   
 <?php } ?>
-</div>
