01ac901633c2d680262cde3d55cbc6e2cc1ead40
Anish Aggarwal <misternyce@gmail.com>
2012-08-15 15:38:26 -0400

    asdfa

--- modules/custom/fm_shop/fm_shop_page_grid_view_item.tpl.php
+++ modules/custom/fm_shop/fm_shop_page_grid_view_item.tpl.php
@@ -7,10 +7,10 @@
     $shop = fm_commerce_get_store($product);
     
     $query_array = array_merge($argument ,array('nid' => $node->nid));
-    $link = l($img, 'node/' . $node->nid, array('html' => true, 'attributes' => array('id' => $node->nid,), 'query' => $query_array ));
+    $link = l($img, 'node/' . $node->nid, array('html' => true, 'attributes' => array('id' => $node->nid, 'class' => array('thumb_link')), 'query' => $query_array ));
     $link .= '<div class="social_favorites">' . fm_social_favorite_get_button($product->product_id, false) . '</div>'; 
     
-    print '<div class="thumb_link">' . $link . '</div>';
+    print '<div class="thumb_img">' . $link . '</div>';
     
     $result = db_query('SELECT count(*) as count FROM fm_social_favorite WHERE product_id = :product_id', array(':product_id' => $product->product_id));
   $obj = $result->fetchObject();
