63c2fbd7abc8996ba7a3fc7e5f4c71ae5b0956a8
Anish Aggarwal <misternyce@gmail.com>
2012-08-17 11:19:20 -0400

    push changes

--- modules/custom/fm_users/fm_users_register/fm_users_register_footer_tabs_promo.tpl.php
+++ modules/custom/fm_users/fm_users_register/fm_users_register_footer_tabs_promo.tpl.php
@@ -4,7 +4,7 @@
 <h2>Get $10 instant credits + an additional $10 after the first purchase of friends you invite.</h2>
 </div>
 <div class="register">
-  <a class="button" href="javascript:void(0);" onClick="fmUserStateRestart(); jQuery('.state1').show();fmForceRegister();fmClearRegisterTimeout();">Sign Up</a>
+  <a class="button" href="javascript:void(0);" onClick="fmUserStateRestart(); jQuery('.state1').show();fmForceRegister();fmClearRegisterTimeout();">Join Free</a>
   <div class="login">Already a Member? <br clear="all"><a href="javascript:void(0);" onClick="fmUserStateRestart(); jQuery('.state3').show();fmForceRegister();fmClearRegisterTimeout();">Login</a></div> 
   
   
