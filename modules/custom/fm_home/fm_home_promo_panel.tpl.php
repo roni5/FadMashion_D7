0e4abc7aac4e4447104571d8d301a383b852c464
Anish Aggarwal <misternyce@gmail.com>
2012-08-20 22:21:24 -0400

    push changes

--- modules/custom/fm_home/fm_home_promo_panel.tpl.php
+++ modules/custom/fm_home/fm_home_promo_panel.tpl.php
@@ -2,6 +2,6 @@
   <div class="panel-block">
     <img src="<?php print pp();?>panel_label_whats.png" alt="What's your New York style?" class="label">
     <p>Fadmashion introduces you to the unique styles of New York City neighborhoods.</p>
-    <a href="#" class="button">Get Inspired</a>
+    <?php print l('Get Inspired', 'shop')?>
   </div>
 </div>
