f1728bf0aef0fa9e4fd53a0ba1b0c04f0575ea04
Anish Aggarwal <misternyce@gmail.com>
2012-08-24 16:19:20 -0400

    push changes

--- modules/custom/fm_shop/fm_shop.js
+++ modules/custom/fm_shop/fm_shop.js
@@ -501,7 +501,7 @@ function item_content(thisNav) {
 	  var nodeContent = jQuery('.product_content #node_' + nid);
 	  var itemName = jQuery('.itemName', nodeContent).html();
 	
-	  return '<span>' + title + '</span><div>' + itemName + '</div>';
+	  return '<div>' + itemName + '</div>';
 	} else {
 		return '';
 	}
