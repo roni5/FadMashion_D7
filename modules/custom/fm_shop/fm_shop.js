fd8e318b35140f51faab7f58c56650e6f76bcf6f
Anish Aggarwal <misternyce@gmail.com>
2012-08-24 20:53:03 -0400

    asdfasdf

--- modules/custom/fm_shop/fm_shop.js
+++ modules/custom/fm_shop/fm_shop.js
@@ -445,7 +445,7 @@ jQuery(document).ready(function() {
 		});
 		
 		//Header Beautytips
-		jQuery('#item_nav a').once('item_nav_bt_active').bt({
+		jQuery('#item_nav a').bt({
 	      contentSelector: "item_content(jQuery(this));",
 		  trigger: 'hover',
 		  positions: ['top'],
