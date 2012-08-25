21cc9c0894b7d2357ccc195a4108ec76b041a03a
Anish Aggarwal <misternyce@gmail.com>
2012-08-24 23:31:47 -0400

    asdfaa

--- themes/v15_fadmashion_commerce/js/fm_onload.js
+++ themes/v15_fadmashion_commerce/js/fm_onload.js
@@ -66,7 +66,7 @@ jQuery(document).ready(function() {
 	  spikeLength: 6,
       padding: '5px',
 	  shrinkToFit: false,
-	  width: '430px',
+	  width: '410px',
 	  cssClass: 'tooltip',
 	  showTip: function(box) {
 
