a45010b2c8ce7952d8447a904e5e9c057038f77b
Anish Aggarwal <misternyce@gmail.com>
2012-08-26 15:20:50 -0400

    asdfasdf

--- modules/custom/fm_users/fm_users_invite_block.tpl.php
+++ modules/custom/fm_users/fm_users_invite_block.tpl.php
@@ -4,11 +4,11 @@
 <h1>Share with Friends, Get Rewarded</h1>
 <div id="explanation">
 <div id="col1">
+<h3 style="margin: 0px 0px 10px; font-size: 14px; border-bottom: 1px dotted rgb(221, 221, 221); padding-bottom: 10px;">You currently have <span style="font-size: 20px; color: #A6352F">$0</span> worth of credits</h3>
    <h2>Friends with Benefits</h2>
   <p>Invite 10 friends to join, and we will give you $30 worth of free credits for any purchases in our boutique.  As more of your friends join, we will give you more credits as a thank you for helping us grow.</p>
 	<br><br>
 	<div class="credits">
-<h1>You have<span><?php print fm_users_credits_get()?></span>worth of credits</h1>
 </div>
 
 </div>
