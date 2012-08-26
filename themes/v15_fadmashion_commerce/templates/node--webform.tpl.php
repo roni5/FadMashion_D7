b1613a0e1dda6902858da717547faf4580b6b840
Anish Aggarwal <misternyce@gmail.com>
2012-08-25 20:06:59 -0400

    asdfasdf

--- themes/v15_fadmashion_commerce/templates/node--webform.tpl.php
+++ themes/v15_fadmashion_commerce/templates/node--webform.tpl.php
@@ -1,4 +1,10 @@
 <div class="contentPanel" id="node_<?php print $node->nid?>">
 <div class="pad">
+<div class="content_col1">
+<?php print render($content['body']); ?>
+<?php print render($content['webform']); ?>
+</div>
+<div class="content_col2">
 <?php print render($content); ?>
+</div>
 </div></div>
