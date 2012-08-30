0a09c4f441c0dda49059c288d05358ea39820058
Anish Aggarwal <misternyce@gmail.com>
2012-08-30 16:23:09 -0400

    asdfasdf

--- modules/custom/fm_shop/fm_shop.js
+++ modules/custom/fm_shop/fm_shop.js
@@ -173,7 +173,7 @@ jQuery(document).ready(function() {
  				jQuery(this).removeClass('ad-active');
  		  });
           
-          jQuery('.contentPanel #collection_viewer_' + nid + ' a.thumb_link').addClass('ad-active');
+          jQuery('.contentPanel #' + nid).addClass('ad-active');
           
           
           try {
