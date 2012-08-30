e60d87322a86c16f2263d4368c4e1aa3cb7b1c7c
Anish Aggarwal <misternyce@gmail.com>
2012-08-29 22:52:27 -0400

    asdfasdf

--- modules/custom/fm_shop/fm_shop.js
+++ modules/custom/fm_shop/fm_shop.js
@@ -410,6 +410,15 @@ function mycarousel_itemAddCallback(carousel, first, last, json)
         carousel.add(count + i, jQuery(this).html());
     });
     jQuery('.contentPanel .product_content').append(json.node_content);
+    
+    jQuery('.col2 .ad-thumb-list li a').live('click', function() {
+			jQuery('.col2 .ad-active').each(function() {
+				jQuery(this).removeClass('ad-active');
+			});
+			jQuery(this).addClass('ad-active');
+        
+		});
+    
 };
 
 
