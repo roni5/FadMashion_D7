30cdcc55dda958accd7a7b556df854acc3298aa6
Anish Aggarwal <misternyce@gmail.com>
2012-08-31 13:49:13 -0400

    push changes

--- modules/custom/fm_deals/fm_deals.js
+++ modules/custom/fm_deals/fm_deals.js
@@ -6,7 +6,7 @@
 function fm_deals_facebookshare( url, thumb_path, description, name, message) {
 	 FB.ui({
        method: 'send',
-	   app_id: '213872101957329',
+	   app_id: appId,
 	   name: name,
 	   link: url,
 	   picture: thumb_path,
--- modules/custom/fm_social/fm_social_facebook/fm_social_facebook.js
+++ modules/custom/fm_social/fm_social_facebook/fm_social_facebook.js
@@ -1,3 +1,65 @@
 /**
  * 
  */
+
+var appId = '213872101957329';
+
+jQuery(document).ready(function() {
+	
+	
+});
+
+window.fbAsyncInit = function() {
+    FB.init({ appId: '113700398662301', 
+        status: true, 
+        cookie: true,
+        xfbml: true,
+        oauth: true});
+
+   showLoader(true);
+   
+   function updateButton(response) {
+        button       =   document.getElementById('fb-auth');
+        userInfo     =   document.getElementById('user-info');
+        
+        if (response.authResponse) {
+            //user is already logged in and connected
+            FB.api('/me', function(info) {
+                login(response, info);
+            });
+            
+            button.onclick = function() {
+                FB.logout(function(response) {
+                    logout(response);
+                });
+            };
+        } else {
+            //user is not connected to your app or logged out
+            button.innerHTML = 'Login';
+            button.onclick = function() {
+                showLoader(true);
+                FB.login(function(response) {
+                    if (response.authResponse) {
+                        FB.api('/me', function(info) {
+                            login(response, info);
+                        });	   
+                    } else {
+                        //user cancelled login or did not grant authorization
+                        showLoader(false);
+                    }
+                }, {scope:'email,user_birthday,status_update,publish_stream,user_about_me'});  	
+            }
+        }
+    }
+    
+    // run once with current status and whenever the status changes
+    FB.getLoginStatus(updateButton);
+    FB.Event.subscribe('auth.statusChange', updateButton);	
+};
+
+(function() {
+    var e = document.createElement('script'); e.async = true;
+    e.src = document.location.protocol 
+        + '//connect.facebook.net/en_US/all.js';
+    document.getElementById('fb-root').appendChild(e);
+}());
