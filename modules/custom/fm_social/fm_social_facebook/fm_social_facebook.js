18ca3cf99f11f276f43deca616afe4fd00e133ca
Anish Aggarwal <misternyce@gmail.com>
2012-09-01 16:17:32 -0400

    asdfasdf

--- modules/custom/fm_social/fm_social_facebook/fm_social_facebook.js
+++ modules/custom/fm_social/fm_social_facebook/fm_social_facebook.js
@@ -3,4 +3,21 @@
  */
 
 
+jQuery(document).ready(function() {
+	window.fbAsyncInit = function() {
+	    FB.init(Drupal.settings.fb.fb_init_settings);
+		
+	   showLoader(true);
+	   
+	   // run once with current status and whenever the status changes
+	   FB.getLoginStatus(updateButton);
+	   FB.Event.subscribe('auth.statusChange', updateButton);	
+	};
+	
+});
 
+
+function updateButton(response) {
+   FB_JS.initFinal(response);
+   FB_JS.authResponseChange(response);
+}
