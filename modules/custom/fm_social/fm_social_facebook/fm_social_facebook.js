9d8af64c4512dc1adaf5b794412ba296cfd9f96d
Anish Aggarwal <misternyce@gmail.com>
2012-09-01 16:18:42 -0400

    asdfasdf

--- modules/custom/fm_social/fm_social_facebook/fm_social_facebook.js
+++ modules/custom/fm_social/fm_social_facebook/fm_social_facebook.js
@@ -2,7 +2,6 @@
  * 
  */
 
-
 jQuery(document).ready(function() {
 	window.fbAsyncInit = function() {
 	    FB.init(Drupal.settings.fb.fb_init_settings);
@@ -20,4 +19,62 @@ jQuery(document).ready(function() {
 function updateButton(response) {
    FB_JS.initFinal(response);
    FB_JS.authResponseChange(response);
+     
+    button       =   document.getElementById('fb-auth');
+    userInfo     =   document.getElementById('user-info');
+    
+    if (response.authResponse) {
+        //user is already logged in and connected
+        FB.api('/me', function(info) {
+            login(response, info);
+        });
+        
+        button.onclick = function() {
+            FB.logout(function(response) {
+                logout(response);
+            });
+            
+        };
+        
+    } else {
+        //user is not connected to your app or logged out
+        button.innerHTML = 'Login';
+        button.onclick = function() {
+            showLoader(true);
+            FB.login(function(response) {
+                if (response.authResponse) {
+                    FB.api('/me', function(info) {
+                        login(response, info);
+                    });	   
+                    	  
+                } else {
+                    //user cancelled login or did not grant authorization
+                    showLoader(false);
+                }
+            }, {scope:'email,user_birthday,status_update,publish_stream,user_about_me'});  	
+        }
+    }
 }
+
+function login(response, info){
+    if (response.authResponse) {
+        var accessToken                                 =   response.authResponse.accessToken;
+        button.innerHTML                               = 'Logout';
+        showLoader(false);
+
+    }
+    
+}
+
+function logout(response){
+    
+}
+
+
+function showLoader(status){
+    if (status)
+        document.getElementById('loader').style.display = 'block';
+    else
+        document.getElementById('loader').style.display = 'none';
+}
+
--- themes/v15_fadmashion_commerce/templates/page.tpl.php
+++ themes/v15_fadmashion_commerce/templates/page.tpl.php
@@ -1,4 +1,25 @@
-
+<div id="fb-root"></div>
+<button id="fb-auth">Login</button>
+        <div id="loader" style="display:none">
+            <img src="<?php print pp()?>loader-red.gif" alt="loading" />
+        </div>
+        <br />
+        <div id="user-info"></div>
+        <br />
+        <div id="debug"></div>
+        
+        <div id="other" style="display:none">
+            <a href="#" onclick="showStream(); return false;">Publish Wall Post</a> |
+            <a href="#" onclick="share(); return false;">Share With Your Friends</a> |
+            <a href="#" onclick="graphStreamPublish(); return false;">Publish Stream Using Graph API</a> |
+            <a href="#" onclick="fqlQuery(); return false;">FQL Query Example</a>
+            
+            <br />
+            <textarea id="status" cols="50" rows="5">Write your status here and click 'Status Set Using Legacy Api Call'</textarea>
+            <br />
+            <a href="#" onclick="setStatus(); return false;">Status Set Using Legacy Api Call</a>
+        </div>
+        
         
 <div id="wrapper">
 	<div id="container">
