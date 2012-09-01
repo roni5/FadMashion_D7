ce58edee8d5256d4cae2e79b57ce15318e6ca9fb
Anish Aggarwal <misternyce@gmail.com>
2012-09-01 15:03:52 -0400

    asdfasdf

--- modules/custom/fm_social/fm_social_facebook/fm_social_facebook.js
+++ modules/custom/fm_social/fm_social_facebook/fm_social_facebook.js
@@ -19,51 +19,10 @@ jQuery(document).ready(function() {
 function updateButton(response) {
    FB_JS.initFinal(response);
    FB_JS.authResponseChange(response);
-     
-    button       =   document.getElementById('fb-auth');
-    userInfo     =   document.getElementById('user-info');
-    
-    if (response.authResponse) {
-        //user is already logged in and connected
-        FB.api('/me', function(info) {
-            login(response, info);
-        });
-        
-        button.onclick = function() {
-            FB.logout(function(response) {
-                logout(response);
-            });
-            
-        };
-        
-    } else {
-        //user is not connected to your app or logged out
-        button.innerHTML = 'Login';
-        button.onclick = function() {
-            showLoader(true);
-            FB.login(function(response) {
-                if (response.authResponse) {
-                    FB.api('/me', function(info) {
-                        login(response, info);
-                    });	   
-                    	  
-                } else {
-                    //user cancelled login or did not grant authorization
-                    showLoader(false);
-                }
-            }, {scope:'email,user_birthday,status_update,publish_stream,user_about_me'});  	
-        }
-    }
+   
 }
 
 function login(response, info){
-    if (response.authResponse) {
-        var accessToken                                 =   response.authResponse.accessToken;
-        button.innerHTML                               = 'Logout';
-        showLoader(false);
-
-    }
-    
 }
 
 function logout(response){
