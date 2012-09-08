3cc9dcee01ea2e7953b0cad1fd25bc0bc2dd3e36
Anish Aggarwal <misternyce@gmail.com>
2012-09-08 17:21:49 -0400

    asdfadsf

--- modules/custom/fm_commerce/fm_commerce_express_checkout/fm_commerce_express_checkout_login.tpl.php
+++ modules/custom/fm_commerce/fm_commerce_express_checkout/fm_commerce_express_checkout_login.tpl.php
@@ -9,13 +9,13 @@
     
     <div class="login_col">
      <h3>Existing Customers</h3>
-              <div class="register_button fb-auth">Login With Facebook</div>
+              <div style="width: auto;" class="register_button fb-auth">Login With Facebook</div>
         <div class="facebook_connecting" style="display: none;">
 <div id="facebook_loader"><img src="<?php print pp()?>loader-red.gif"></div>
 <div style="margin-top: 10px;" >Connecting with Facebook</div>
 </div>  
      
-     <br> OR <br>
+     <img style="margin: 10px 0" src="<?php print pp();?>divider_or.png">
     <div style="margin-top: 10px;"><?php print render(drupal_get_form('user_login_block'));?></div>
     </div>
     <div style="display: none;" class="forgot_password">
