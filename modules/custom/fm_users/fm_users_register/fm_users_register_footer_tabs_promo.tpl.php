b90aba3ddba039687e1938327f07475ce8dcc824
Anish Aggarwal <misternyce@gmail.com>
2012-09-09 17:50:30 -0400

    asdfa

--- modules/custom/fm_users/fm_users_register/fm_users_register_footer_tabs_promo.tpl.php
+++ modules/custom/fm_users/fm_users_register/fm_users_register_footer_tabs_promo.tpl.php
@@ -3,9 +3,10 @@
 <h1><span>Insider Access</span> to New York's Hottest Designer Styles. </h1>
 <h2>Get $10 instant credits + an additional $30 for inviting 10 of your friends.</h2>
 </div>
+
 <div class="register">
   <div style="float: left" class=" facebookBtnSmall fb-auth">Join Free</div>
-  <div class="login">Already a Member? <br clear="all"><a href="javascript:void(0);" onClick="jQuery('#login').trigger('click')">Login</a></div> 
+  <div class="login_link">Already a Member? <br clear="all"><a href="javascript:void(0);" onClick="jQuery('#login').trigger('click')">Login</a></div> 
   
   
 </div>
