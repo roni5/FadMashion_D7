f10ef40fa7becbc329ce6eb8c95b1c07acc9c29a
Anish Aggarwal <misternyce@gmail.com>
2012-09-08 23:07:06 -0400

    asdfasdf

--- themes/v15_fadmashion_commerce/templates/header-blog.tpl.php
+++ themes/v15_fadmashion_commerce/templates/header-blog.tpl.php
@@ -41,7 +41,7 @@
       <ul>
         <li class="<?php $is_front ? print 'active' : ''?>"><?php print l('Home', 'home', array('attributes' => array('class' => array('home') )));?></li>
         <li class="<?php arg(0) == 'shop' ? print 'active' : ''?>"><a href="/shop" class="tooltipBtn">Shop</a></li>
-         <?php print fm_shop_subnav_items();?>
+         <?php //print fm_shop_subnav_items();?>
          <?php global $user;
             if ($user->uid) { ?>
          <li class="<?php (arg(0) == 'user' && arg(1) == 'share-with-friends') ? print 'active' : ''?>"><?php print l('Share', 'user/share-with-friends')?></li>
