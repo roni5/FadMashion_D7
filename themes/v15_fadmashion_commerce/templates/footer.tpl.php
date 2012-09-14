0e1f354275027a2522d0277f1e0eec92fda243ce
Anish Aggarwal <misternyce@gmail.com>
2012-09-13 22:29:19 -0400

    asdfasdf

--- themes/v15_fadmashion_commerce/templates/footer.tpl.php
+++ themes/v15_fadmashion_commerce/templates/footer.tpl.php
@@ -3,7 +3,20 @@
 <?php if(!empty($page['footer'])): ?>
 <div class="rounded-top" id="footer">
 
-  			  <?php print render($page['footer']);?>
+<div class="copyright">
+ ©  <img height="40px" src="<?php print pp();?>logo.png" alt="<?php print t('Fadmashion'); ?>" />
+</div>			  
+<?php //print render($page['footer']);?>
+<?php  $url = url('static', array('alias' => true, 'fragment' => '!')); ?>
+<div class="links">
+<a href="<?php print $url?>/about-us">We ♥ New York</a>
+<a href="<?php print $url?>/faq">What is Fadmashion?</a>
+<a href="<?php print $url?>/faq">Returns & Exchanges</a>
+<a href="<?php print $url?>/faq">Shipping Info</a>
+<a href="<?php print $url?>/contact-us">Contact Us</a>
+<a href="http://blog.fadmashion.com">Blog</a>
+</div>
+
 </div>
 <?php endif;  ?> 
  
