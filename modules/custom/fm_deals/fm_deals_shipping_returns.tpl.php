8470019f7d7e34940f5e81cafc4d14fc620be9bf
aaggarwa88 <misternyce@gmail.com>
2012-10-18 15:00:41 -0400

    push changes

--- modules/custom/fm_deals/fm_deals_shipping_returns.tpl.php
+++ modules/custom/fm_deals/fm_deals_shipping_returns.tpl.php
@@ -1,11 +1,9 @@
 
 <?php if ($isShipping) {?>
-<p><b>$9.99</b> Shipping Anywhere in the US</p>
-<p>Shipping Time: <b>2-3 weeks</b></p>
-<p style="margin-top: 20px;">Read more about <?php print l('Shipping', 'faq');?> in our FAQ's</p>
+<p><b>FREE</b> Shipping Anywhere in the US</p>
+<p>Shipping Time: <b>1-2 weeks</b></p>
 <?php } else {?>
-<p>Due to the high discounts on in-season items that we negotiate with our designer partners, all sales are final; however exceptions may be made in the case of defective items or size change requests. For all purchase-related questions, please contact us directly and we will reply as soon as possible.
+<p>Fadmashion offers a no-hassle return policy. Contact us anytime if you have an issue with your order and we will gladly refund your money.  
 </p>
-<p style="margin-top: 20px;">Still have questions?  Read about <?php print l('Returns', 'faq');?> in our FAQ's</p>
 <?php }?>
 
